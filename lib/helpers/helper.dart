a5634672c79a5247ed1faf301903cd92ac257f68
Jp Siegel <jpsiegel8@gmail.com>
2022-04-17 23:06:05 -0400

    Notification helper

--- lib/helpers/helper.dart
+++ lib/helpers/helper.dart
@@ -6,27 +6,4 @@ export 'package:phitnest/helpers/authentication/authentication_helper.dart';
 export 'package:phitnest/helpers/location/location_helper.dart';
 export 'package:phitnest/helpers/helper.dart';
 export 'package:phitnest/helpers/navigation/navigation_helper.dart';
-
-import 'dart:convert';
-
-import 'package:phitnest/constants/constants.dart';
-import 'package:http/http.dart' as http;
-
-sendNotification(String token, String title, String body,
-    Map<String, dynamic>? payload) async {
-  await http.post(
-    Uri.parse('https://fcm.googleapis.com/fcm/send'),
-    headers: <String, String>{
-      'Content-Type': 'application/json',
-      'Authorization': 'key=$SERVER_KEY',
-    },
-    body: jsonEncode(
-      <String, dynamic>{
-        'notification': <String, dynamic>{'body': body, 'title': title},
-        'priority': 'high',
-        'data': payload ?? <String, dynamic>{},
-        'to': token
-      },
-    ),
-  );
-}
+export 'package:phitnest/helpers/notifications/notification_helper.dart';
