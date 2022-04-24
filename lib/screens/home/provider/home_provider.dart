c245d2ce089817664a7542c10fb7a5915531a5b1
Jp Siegel <jpsiegel8@gmail.com>
2022-04-24 11:42:03 -0400

    Improved state management, fixed firebase error on logout

---
+++ lib/screens/home/provider/home_provider.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/models.dart';
+import '../../screens.dart';
+
+enum Selection { Conversations, Profile, Swipe }
+
+class ScreenState extends ChangeNotifier {
+  final UserModel user;
+
+  late final Map<Selection, Widget> _widgets;
+
+  Selection _selection = Selection.Swipe;
+
+  Selection get selection => _selection;
+
+  set selection(Selection selection) {
+    _selection = selection;
+    notifyListeners();
+  }
+
+  Widget get currentWidget => _widgets[_selection]!;
+
+  ScreenState({required this.user}) : super() {
+    _widgets = {
+      Selection.Swipe: SwipeScreen(user: user),
+      Selection.Conversations: ConversationsScreen(user: user),
+      Selection.Profile: ProfileScreen(user: user)
+    };
+  }
+}
+
+class HomeScreenProvider extends StatelessWidget {
+  final UserModel user;
+  final Widget Function(BuildContext context, ScreenState state, Widget? child)
+      builder;
+
+  const HomeScreenProvider(
+      {Key? key, required this.user, required this.builder})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => ScreenState(user: user),
+        child: Consumer<ScreenState>(builder: builder));
+  }
+}
