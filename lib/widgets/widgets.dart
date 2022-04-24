354c0257d03dd2b6dd8822f7b189d5d587b2260c
Jp Siegel <jpsiegel8@gmail.com>
2022-04-23 20:27:58 -0400

    More state management

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,3 @@
 export 'package:phitnest/widgets/playerWidget/player_widget.dart';
 export 'package:phitnest/widgets/tinderCard/custom_flutter_tinder_card_widget.dart';
-export 'package:phitnest/widgets/redirectorWidget/redirector_widget.dart';
+export 'package:phitnest/screens/redirector/redirector_screen.dart';
