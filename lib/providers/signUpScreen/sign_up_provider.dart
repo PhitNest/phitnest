94d48b617cc5ade01dd7e69c097bd45066d8ae61
Jp Siegel <jpsiegel8@gmail.com>
2022-04-23 21:36:18 -0400

    Improved state management

--- lib/providers/signUpScreen/sign_up_provider.dart
+++ lib/providers/signUpScreen/sign_up_provider.dart
@@ -3,8 +3,22 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
-class SignUpScreenModel extends ChangeNotifier {
+class SignUpScreenProvider extends StatelessWidget {
+  final Widget Function(BuildContext, ScreenState, Widget?) builder;
+
+  SignUpScreenProvider({Key? key, required this.builder}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => ScreenState(),
+        child: Consumer<ScreenState>(builder: builder));
+  }
+}
+
+class ScreenState extends ChangeNotifier {
   final ImagePicker imagePicker = ImagePicker();
   File? _image;
   TextEditingController passwordController = TextEditingController();
