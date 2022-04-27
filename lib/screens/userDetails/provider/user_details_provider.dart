0e9509bc1446dd057c7879e16197a4252116f557
Jp Siegel <jpsiegel8@gmail.com>
2022-04-27 13:20:38 -0400

    Updated user details screen

---
+++ lib/screens/userDetails/provider/user_details_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:phitnest/screens/userDetails/provider/src/user_details_functions.dart';
+import 'package:provider/provider.dart';
+
+import '../../../models/models.dart';
+import '../../../services/services.dart';
+import 'src/user_details_functions.dart';
+import 'src/user_details_model.dart';
+
+class UserDetailsProvider extends StatelessWidget {
+  final Widget Function(
+      BuildContext context,
+      UserModel user,
+      UserDetailsModel model,
+      UserDetailsFunctions functions,
+      Widget? child) builder;
+
+  UserDetailsProvider({Key? key, required this.builder}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        create: (context) => UserDetailsModel(),
+        child: Consumer<UserDetailsModel>(builder: ((context, model, child) {
+          UserModel? user = BackEndModel.getBackEnd(context).currentUser!;
+          return builder(
+              context,
+              user,
+              model,
+              UserDetailsFunctions(context: context, user: user, model: model),
+              child);
+        })));
+  }
+}
