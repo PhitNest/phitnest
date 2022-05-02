90b631cd33b5b119d89874907be90c69afa2f949
Jp Siegel <jpsiegel8@gmail.com>
2022-05-02 19:03:14 -0400

    Constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,22 @@
-library constants;
+// Logo colors
+const LOGO_INNER_RING = 0xFF206D81;
+const LOGO_MIDDLE_RING = 0xFF289D9F;
+const LOGO_OUTER_RING = 0xFF4FB3AF;
+const LOGO_TEXT = 0xFFA3A3A3;
+const LOGO_BACKGROUND_GRAY = 0xFFDDDDDD;
+const LOGO_RED = 0xFFD53C31;
+const LOGO_ORANGE = 0xFFDE722D;
+const LOGO_YELLOW = 0xFFEBAF29;
+const LOGO_TAN = 0xFFECD4A7;
 
 const FINISHED_ON_BOARDING = 'finishedOnBoarding';
 const COLOR_ACCENT = 0xFFff8c9b;
 const COLOR_PRIMARY_DARK = 0xFFb42442;
-const COLOR_PRIMARY = 0xFFeb5a6d;
+const COLOR_PRIMARY = LOGO_OUTER_RING;
 const FACEBOOK_BUTTON_COLOR = 0xFF415893;
+const SECOND_MILLIS = 1000;
+const MINUTE_MILLIS = 60 * SECOND_MILLIS;
+const HOUR_MILLIS = 60 * MINUTE_MILLIS;
 const USERS = 'users';
 const SWIPES = 'swipes';
 const CHANNEL_PARTICIPATION = 'channel_participation';
@@ -12,9 +24,6 @@ const CHANNELS = 'channels';
 const THREAD = 'thread';
 const REPORTS = 'reports';
 const SUBSCRIPTIONS = 'subscriptions';
-const SECOND_MILLIS = 1000;
-const MINUTE_MILLIS = 60 * SECOND_MILLIS;
-const HOUR_MILLIS = 60 * MINUTE_MILLIS;
 const SWIPE_COUNT = 'swipe_counts';
 const MONTHLY_SUBSCRIPTION = 'io.instamobile.flutter.android.plan.monthly';
 const YEARLY_SUBSCRIPTION = 'io.instamobile.flutter.android.plan.yearly';
