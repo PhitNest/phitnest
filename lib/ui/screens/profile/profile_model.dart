7da9e597bbbe63924b3fb99432486bd669e20258
Jp Siegel <jpsiegel8@gmail.com>
2022-05-08 17:20:26 -0400

    Last major code cleanup

--- lib/ui/screens/profile/profile_model.dart
+++ lib/ui/screens/profile/profile_model.dart
@@ -1,10 +1,3 @@
-import 'package:flutter/material.dart';
-
 import '../models.dart';
 
-class ProfileModel extends BaseModel {
-  List images = [];
-  List pages = [];
-  List<Widget> gridPages = [];
-  PageController controller = PageController();
-}
+class ProfileModel extends BaseModel {}
--- lib/ui/screens/profile/profile_provider.dart
+++ lib/ui/screens/profile/profile_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../models/models.dart';
 import '../providers.dart';
 import 'profile_model.dart';
 import 'profile_view.dart';
@@ -9,18 +8,5 @@ class ProfileProvider extends AuthenticatedProvider<ProfileModel, ProfileView> {
   ProfileProvider({Key? key}) : super(key: key);
 
   @override
-  init(BuildContext context, ProfileModel model) async =>
-      await super.init(context, model) && await loadUserData(model);
-
-  @override
   ProfileView build(BuildContext context, ProfileModel model) => ProfileView();
-
-  loadUserData(ProfileModel model) async {
-    UserModel? user = authService.userModel;
-    if (user != null) {
-      model.images.clear();
-      model.images.addAll(user.photos);
-      model.images.add(null);
-    }
-  }
 }
