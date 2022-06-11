b4396ae48411e229dd5d45092e6d876c7ee45bf8
Jp Siegel <jpsiegel8@gmail.com>
2022-06-11 16:27:11 -0400

    Organizational change + firebase messaging package setup

--- lib/utils/firebase/firebase_chat_service.dart
+++ lib/utils/firebase/firebase_chat_service.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 import '../../models/models.dart';
 import '../services.dart';
 
 class FirebaseChatService extends ChatService {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  Future<bool> requestNotificationPermissions() async =>
+      (await messaging.requestPermission()).authorizationStatus ==
+      AuthorizationStatus.authorized;
+
   Future<bool> refreshConnection() async {
     return false;
   }
