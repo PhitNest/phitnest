66dca1c6964597735306139c797f713a20cc583a
Jp Siegel <jpsiegel8@gmail.com>
2022-06-11 16:27:11 -0400

    Organizational change + firebase messaging package setup

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,6 +1,8 @@
 import '../models/models.dart';
 
 abstract class ChatService {
+  Future<bool> requestNotificationPermissions();
+
   Future<bool> refreshConnection();
 
   Future<bool> sendMessage(UserModel from, UserModel to);
---
+++ lib/services/rest/rest_service.dart
@@ -0,0 +1,3 @@
+export 'authentication_service_implementation.dart';
+export 'database_service_implementation.dart';
+export 'storage_service_implementation.dart';
