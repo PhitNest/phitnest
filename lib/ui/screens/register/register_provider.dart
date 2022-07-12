a09f704da61547e4372827fa15c5bb34ec35e0ab
Jp <jpsiegel8@gmail.com>
2022-07-11 21:00:56 -0400

    Fixed date form field

--- lib/ui/screens/register/register_provider.dart
+++ lib/ui/screens/register/register_provider.dart
@@ -87,8 +87,7 @@ class RegisterProvider
           firstName: model.firstNameController.text.trim(),
           lastName: model.lastNameController.text.trim(),
           mobile: model.mobileController.text.trim(),
-          birthday: DateTime
-              .now(), // DateTime.parse(model.dateOfBirthController.text.trim()),
+          birthday: DateTime.parse(model.dateOfBirthController.text.trim()),
           ip: await userIP,
           profilePicture: model.image,
           locationData: position == null
