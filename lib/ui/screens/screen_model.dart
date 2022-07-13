b1fbfb2804db7cf7ac4ffb62a631c9ee52d47f19
Jp <jpsiegel8@gmail.com>
2022-07-13 15:23:10 -0400

    Updated provider model view, don't ever use the screen provider for common widgets. You can't have two of the same screen provider subclass in the widget tree at once or you will get very cryptic error messages.

--- lib/ui/screens/screen_model.dart
+++ lib/ui/screens/screen_model.dart
@@ -1,6 +1,19 @@
-import '../base_model.dart';
+import 'package:flutter/material.dart';
 
-abstract class ScreenModel extends BaseModel {
-  ScreenModel({bool initiallyLoading = true})
-      : super(initiallyLoading: initiallyLoading);
+abstract class ScreenModel extends ChangeNotifier {
+  bool _loading;
+
+  /// Setting initiallyLoading to true will cause the loading widget to be
+  /// built. Setting [loading] to false will rebuild the view.
+  ScreenModel({bool initiallyLoading = true}) : _loading = initiallyLoading;
+
+  bool get loading => _loading;
+
+  /// Updating the value of [loading] to a new value will rebuild the view
+  set loading(bool loading) {
+    if (_loading != loading) {
+      _loading = loading;
+      notifyListeners();
+    }
+  }
 }
