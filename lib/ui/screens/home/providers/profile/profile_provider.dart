87cb1c075ae4d4963ecdf3c5d32cf0d2f4d1d554
Jp <jpsiegel8@gmail.com>
2022-07-14 22:52:18 -0400

    Refactoring

--- lib/ui/screens/home/providers/profile/profile_provider.dart
+++ lib/ui/screens/home/providers/profile/profile_provider.dart
@@ -1,29 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../models/models.dart';
 import '../../../screens.dart';
 import '../../models/home_model.dart';
 import '../../views/home_view.dart';
 
 class ProfileProvider extends ScreenProvider<ProfileModel, ProfileView> {
-  final String firstName;
-  final String lastName;
-  final String bio;
-  final String profilePictureUrl;
+  final AuthenticatedUser user;
 
   const ProfileProvider({
     Key? key,
-    required this.firstName,
-    required this.lastName,
-    required this.bio,
-    required this.profilePictureUrl,
+    required this.user,
   }) : super(key: key);
 
   @override
   ProfileView build(BuildContext context, ProfileModel model) => ProfileView(
-        firstName: firstName,
-        lastName: lastName,
-        bio: bio,
-        profilePictureUrl: profilePictureUrl,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        bio: user.bio,
+        profilePictureUrl: user.profilePictureUrl,
       );
 
   @override
