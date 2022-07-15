d099bfdaa412442abfe2bcab82baa540e944a9e2
Jp <jpsiegel8@gmail.com>
2022-07-14 22:58:36 -0400

    Refactored date of birth controller

--- lib/ui/common/widgets/textForm/controllers/date_editing_controller.dart
+++ lib/ui/common/widgets/textForm/controllers/date_editing_controller.dart
@@ -2,12 +2,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 class DateEditingController extends TextEditingController {
-  String dateTimeToString(DateTime dateTime) =>
-      DateFormat('yyyy-MM-dd', 'en_US').format(dateTime).toString();
+  static final DateFormat kFormat = DateFormat('yyyy-MM-dd', 'en_US');
 
-  DateTime get date => DateFormat('yyyy-MM-dd', 'en_US').parse(text);
+  DateTime get date => kFormat.parse(text);
 
   set date(DateTime dateTime) {
-    text = dateTimeToString(dateTime);
+    text = kFormat.format(dateTime).toString();
   }
 }
