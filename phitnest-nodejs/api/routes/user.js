9b0e20ab11f102f639b5fca69a35444a4aafe51e
Jp <jpsiegel8@gmail.com>
2022-07-16 18:48:52 -0400

    More user information

--- phitnest-nodejs/api/routes/user.js
+++ phitnest-nodejs/api/routes/user.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user');
 const duplicateEmail = require('../middleware/auth/checkEmail');
+const duplicateMobile = require('../middleware/auth/checkMobile');
 
-router.post('/register', [duplicateEmail], userController.register);
+router.post('/register', [duplicateEmail, duplicateMobile], userController.register);
 router.post('/login', userController.login);
 router.get('/data', userController.getUser);
 module.exports = router;
