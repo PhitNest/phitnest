e1d37ccf4b7e15d56ecebfd72684f240c41b01a7
Jp <jpsiegel8@gmail.com>
2022-07-17 12:40:31 -0400

    Refactoring

--- phitnest-nodejs/api/controllers/user/getFullData.js
+++ phitnest-nodejs/api/controllers/user/getFullData.js
@@ -2,18 +2,20 @@ const { userModel } = require('../../models/user');
 const errorJson = require('../../../utils/error');
 
 module.exports = async (req, res) => {
-    let user = await userModel.findById(res.locals.jwtData._id).catch((e) => {
-        return res.status(500).json(errorJson(e, 'An error occurred while getting your information, please try again.'))
-    });
-
-    res.status(200).send([{
-        email: user.email,
-        mobile: user.mobile,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        bio: user.bio,
-        friends: user.friends,
-        conversations: user.conversations,
-        online: user.online,
-    }]);
+    try {
+        const user = await userModel.findById(res.locals.jwtData._id)
+        if (user) {
+            res.status(200).send([{
+                email: user.email,
+                mobile: user.mobile,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                bio: user.bio,
+                friends: user.friends,
+                conversations: user.conversations,
+                online: user.online,
+            }]);
+        }
+    } catch (e) { }
+    return res.status(500).json(errorJson(e, 'An error occurred while getting your information, please try again.'))
 }
