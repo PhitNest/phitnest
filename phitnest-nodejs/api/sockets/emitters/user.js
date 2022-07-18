fc295c66de7e00104fd75e558bdfbabeb4ca6cc9
Jp <jpsiegel8@gmail.com>
2022-07-18 19:12:21 -0400

    Refactoring

--- phitnest-nodejs/api/sockets/emitters/user.js
+++ phitnest-nodejs/api/sockets/emitters/user.js
@@ -2,16 +2,14 @@ const { userModel } = require('../../models/user');
 
 module.exports = (io) => {
     userModel.watch().on('change', async (doc) => {
-        try {
-            const user = await userModel.findById(doc.documentKey._id);
-            if (user) {
-                io.emit(`userSubscription:${user._id}`, {
-                    firstName: user.firstName,
-                    lastName: user.lastName,
-                    bio: user.bio,
-                    online: user.online,
-                });
-            }
-        } catch (error) { }
+        const user = await userModel.findById(doc.documentKey._id);
+        if (user) {
+            io.emit(`userSubscription:${user._id}`, {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                bio: user.bio,
+                online: user.online,
+            });
+        }
     });
 };
