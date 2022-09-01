0382d0e3d9d6636988f902d7d20e6e03f46f6320
JP Siegel <jpsiegel8@gmail.com>
2022-09-01 00:04:59 -0400

    Fixed assets

--- phitnest_mobile/lib/src/ui/screens/onBoarding/on_boarding.dart
+++ phitnest_mobile/lib/src/ui/screens/onBoarding/on_boarding.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../../../constants/routes.dart';
-import '../../../repositories/repositories.dart';
 import '../screen.dart';
 import 'on_boarding_state.dart';
 import 'on_boarding_view.dart';
 
 class OnBoardingScreen extends Screen<OnBoardingState, OnBoardingView> {
   const OnBoardingScreen() : super();
-
-  static const Duration splashDuration = Duration(seconds: 1);
-  static const Duration fadeoutDuration = Duration(milliseconds: 500);
-
-  @override
-  Future<void> init(BuildContext context, OnBoardingState state) =>
-      repositories<DeviceCacheRepository>()
-          .completedOnBoarding
-          .then((completed) {
-        if (completed) {}
-      });
 
   @override
   OnBoardingView build(BuildContext context, OnBoardingState state) =>
