c7a4e50c9f09d087f2417d8fbd8f6f4055f2ec72
JP Siegel <jpsiegel8@gmail.com>
2022-09-03 13:01:23 -0400

    Updates

--- backend/lib/controllers/message/listMessages.js
+++ backend/lib/controllers/message/listMessages.js
@@ -1,25 +1,34 @@
 module.exports = async (req, res) => {
-  const messageIds = await res.locals.redis.zrevrange(
-    `conversations:${req.query.conversation}:messages`,
-    0,
-    req.query.limit
-  );
-
-  res.status(200).json(
-    await Promise.all(
-      messageIds.map(async (id) => {
-        const key = `messages:${id}`;
-        const [sender, text] = await res.locals.redis
-          .multi()
-          .hget(key, 'sender')
-          .hget(key, 'text')
-          .exec();
-        return {
-          id: id,
-          sender: sender,
-          text: text,
-        };
-      })
+  if (
+    await res.locals.redis.sismember(
+      `conversations:${req.query.conversation}:participants`,
+      res.locals.userId
     )
-  );
+  ) {
+    const messageIds = await res.locals.redis.zrevrange(
+      `conversations:${req.query.conversation}:messages`,
+      0,
+      req.query.limit
+    );
+
+    res.status(200).json(
+      await Promise.all(
+        messageIds.map(async (id) => {
+          const key = `messages:${id}`;
+          const [sender, text] = await res.locals.redis
+            .multi()
+            .hget(key, 'sender')
+            .hget(key, 'text')
+            .exec();
+          return {
+            id: id,
+            sender: sender,
+            text: text,
+          };
+        })
+      )
+    );
+  } else {
+    res.status(500).send('You are not a memebr of this conversation.');
+  }
 };
--- backend/lib/middleware/message/validateListMessages.js
+++ backend/lib/middleware/message/validateListMessages.js
@@ -1,10 +1,9 @@
 const joi = require('joi');
-const objectId = require('joi-objectid')(joi);
 const { maxListMessageLimit } = require('../../constants');
 
 function validate(req) {
   const schema = joi.object({
-    conversation: objectId().required(),
+    conversation: joi.string().required(),
     limit: joi.number().min(1).max(maxListMessageLimit).required(),
   });
   return schema.validate(req);
