3370285b328228c3a54e875ea350fa12909ad07c
JP Siegel <jpsiegel8@gmail.com>
2022-09-14 19:38:37 -0400

    Scrollable screens

--- phitnest_mobile/lib/src/ui/screens/contactUs/contact_us_view.dart
+++ phitnest_mobile/lib/src/ui/screens/contactUs/contact_us_view.dart
@@ -37,71 +37,75 @@ class ContactUsView extends ScreenView {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-      resizeToAvoidBottomInset: false,
       body: Container(
-        margin: EdgeInsets.only(
-            top: MediaQuery.of(context).size.height * 0.12,
-            bottom: MediaQuery.of(context).size.height * 0.061),
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Text(
-            "How are we doing?",
-            style: Theme.of(context).textTheme.headlineLarge,
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.06),
-          Text(
-            "Your input is invaluable in building\nstronger and healthier\ncommunities together.",
-            style: Theme.of(context).textTheme.labelLarge,
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.06),
-          Container(
-              width: MediaQuery.of(context).size.width * 0.776,
-              child: Form(
-                child: Column(children: [
-                  createTextField(context, 'Name', nameController),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.015),
-                  createTextField(context, 'Email', emailController),
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.063),
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.159,
-                    child: TextField(
-                        controller: feedbackController,
-                        textAlignVertical: TextAlignVertical.top,
-                        textAlign: TextAlign.left,
-                        expands: true,
-                        maxLines: null,
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(16),
-                            hintText: 'Your feedback',
-                            hintStyle: Theme.of(context)
-                                .textTheme
-                                .labelMedium!
-                                .copyWith(color: Color(0xFF999999)),
-                            border: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Color(0xFF999999)),
-                                borderRadius: BorderRadius.circular(8)))),
-                  )
-                ]),
-              )),
-          SizedBox(height: MediaQuery.of(context).size.height * 0.063),
-          StyledButton(
-            context,
-            child: Text('SUBMIT'),
-            onPressed: onPressedSubmit,
-          ),
-          Expanded(child: Container()),
-          TextButton(
-              onPressed: onPressedExit,
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(Colors.transparent)),
-              child: Text('EXIT',
-                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      color: Colors.black,
-                      fontStyle: FontStyle.italic,
-                      decoration: TextDecoration.underline))),
-        ]),
-      ));
+          width: double.infinity,
+          child: SingleChildScrollView(
+              child: Container(
+            margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.12,
+                bottom: MediaQuery.of(context).size.height * 0.061),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              Text(
+                "How are we doing?",
+                style: Theme.of(context).textTheme.headlineLarge,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+              Text(
+                "Your input is invaluable in building\nstronger and healthier\ncommunities together.",
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.06),
+              Container(
+                  width: MediaQuery.of(context).size.width * 0.776,
+                  child: Form(
+                    child: Column(children: [
+                      createTextField(context, 'Name', nameController),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.015),
+                      createTextField(context, 'Email', emailController),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.063),
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.159,
+                        child: TextField(
+                            controller: feedbackController,
+                            textAlignVertical: TextAlignVertical.top,
+                            textAlign: TextAlign.left,
+                            expands: true,
+                            maxLines: null,
+                            decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(16),
+                                hintText: 'Your feedback',
+                                hintStyle: Theme.of(context)
+                                    .textTheme
+                                    .labelMedium!
+                                    .copyWith(color: Color(0xFF999999)),
+                                border: OutlineInputBorder(
+                                    borderSide:
+                                        BorderSide(color: Color(0xFF999999)),
+                                    borderRadius: BorderRadius.circular(8)))),
+                      )
+                    ]),
+                  )),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.063),
+              StyledButton(
+                context,
+                child: Text('SUBMIT'),
+                onPressed: onPressedSubmit,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.0656),
+              TextButton(
+                  onPressed: onPressedExit,
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.transparent)),
+                  child: Text('EXIT',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Colors.black,
+                          fontStyle: FontStyle.italic,
+                          decoration: TextDecoration.underline))),
+            ]),
+          ))));
 }
