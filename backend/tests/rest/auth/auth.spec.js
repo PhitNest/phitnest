e351334b1b8e158fd0fa1ac055fd3d85c327c825
JP Siegel <jpsiegel8@gmail.com>
2022-09-18 16:43:35 -0400

    Admin registration/login

--- backend/tests/rest/auth/auth.spec.js
+++ backend/tests/rest/auth/auth.spec.js
@@ -4,8 +4,12 @@
 
 const testLogin = require('./login');
 const testRegister = require('./register');
+const testAdminLogin = require('./adminLogin');
+const testAdminRegister = require('./adminRegister');
 
 describe('Auth Test Suite', () => {
   describe('Login', testLogin);
   describe('Register', testRegister);
+  describe('Admin Login', testAdminLogin);
+  describe('Admin Register', testAdminRegister);
 });
