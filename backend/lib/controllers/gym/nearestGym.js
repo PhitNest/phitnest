cd7f7da0e732bc4c49746e69b54a8bb84c15336c
JP Siegel <jpsiegel8@gmail.com>
2022-09-19 11:49:02 -0400

    Refactoring

--- backend/lib/controllers/gym/nearestGym.js
+++ backend/lib/controllers/gym/nearestGym.js
@@ -5,9 +5,10 @@ module.exports = async (req, res) => {
     res.locals.redis
   );
 
-  const gym = await getGym(
-    await findNearestGym(req.query.longitude, req.query.latitude, 1000)
-  );
-
-  return res.status(StatusOK).json(gym);
+  const id = await findNearestGym(req.query.longitude, req.query.latitude, 100);
+  if (id) {
+    const gym = await getGym(id);
+    return res.status(StatusOK).json(gym);
+  }
+  return res.status(StatusNoContent).send('You have no nearby gyms');
 };
