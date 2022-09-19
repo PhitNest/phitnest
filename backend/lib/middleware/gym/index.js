e3f26f05714b5263dc1cd64451d87c81081675d8
JP Siegel <jpsiegel8@gmail.com>
2022-09-19 11:34:22 -0400

    Gym searching

--- backend/lib/middleware/gym/index.js
+++ backend/lib/middleware/gym/index.js
@@ -1,5 +1,7 @@
 const validateCreateGym = require('./validateCreateGym');
+const validateNearestGym = require('./validateNearestGym');
 
 module.exports = {
   validateCreateGym: validateCreateGym,
+  validateNearestGym: validateNearestGym,
 };
--- backend/lib/schema/gym.js
+++ backend/lib/schema/gym.js
@@ -17,9 +17,9 @@ module.exports = (redis) => ({
     const [name, city, state, streetAddress, zipCode] = await redis
       .multi()
       .hget(gymKey(id), 'name')
+      .hget(gymKey(id), 'streetAddress')
       .hget(gymKey(id), 'city')
       .hget(gymKey(id), 'state')
-      .hget(gymKey(id), 'streetAddress')
       .hget(gymKey(id), 'zipCode')
       .exec();
     return {
@@ -61,7 +61,10 @@ module.exports = (redis) => ({
       'https://nominatim.openstreetmap.org/search',
       {
         params: {
-          q: `${streetAddress}, ${city}, ${state} ${zipCode}`,
+          q: `${streetAddress}, ${city}, ${state} ${zipCode}`.replace(
+            /%20/g,
+            '+'
+          ),
           format: 'json',
           polygon: 1,
           addressdetails: 1,
@@ -71,11 +74,11 @@ module.exports = (redis) => ({
     const id = await getCount(redis);
     await redis
       .multi()
-      .geoadd(gymsDomain, mapResponse.lon, mapResponse.lat, id)
+      .geoadd(gymsDomain, mapResponse.data[0].lon, mapResponse.data[0].lat, id)
       .hset(gymKey(id), {
         name: name,
-        city: city,
         streetAddress: streetAddress,
+        city: city,
         state: state,
         zipCode: zipCode,
       })
