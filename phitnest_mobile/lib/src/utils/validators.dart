adc019f363ec33623ccc85d65f46bd4ea3ee3138
JP Siegel <jpsiegel8@gmail.com>
2022-09-27 19:32:22 -0400

    email validator

--- phitnest_mobile/lib/src/utils/validators.dart
+++ phitnest_mobile/lib/src/utils/validators.dart
@@ -1,7 +1,8 @@
 const int kMaxFirstNameLength = 24;
+const int kMaxEmailLength = 64;
 
-String? validateFirstName(String name) {
-  if (name.isEmpty) {
+String? validateFirstName(String? name) {
+  if (name == null || name.isEmpty) {
     return 'You must enter a name.';
   }
 
@@ -11,6 +12,24 @@ String? validateFirstName(String name) {
 
   if (RegExp(r"/^[a-z ,.'-]+$/i").hasMatch(name)) {
     return 'Please enter a valid name.';
+  }
+
+  return null;
+}
+
+String? validateEmail(String? email) {
+  if (email == null || email.isEmpty) {
+    return 'You must enter a valid email.';
+  }
+
+  if (email.length > kMaxEmailLength) {
+    return 'Please enter an email shorter than $kMaxEmailLength characters.';
+  }
+
+  if (RegExp(
+          r"^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@↵(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$")
+      .hasMatch(email)) {
+    return 'Please enter a valid email address.';
   }
 
   return null;
