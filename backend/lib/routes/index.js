dff3b77a845772d5c7d0807704af4fb008394b48
Jp <jpsiegel8@gmail.com>
2022-10-02 23:15:13 -0400

    Refactoring

--- backend/lib/routes/index.js
+++ backend/lib/routes/index.js
@@ -5,7 +5,7 @@ const message = require("./message");
 const conversation = require("./conversation");
 const gym = require("./gym");
 
-router.use("/auth", auth);
+router.use(auth);
 router.use(message);
 router.use(conversation);
 router.use(gym);
