06ff493a448a76895c0deb6afe507378296622b1
Jp <jpsiegel8@gmail.com>
2022-10-06 11:55:17 -0400

    Fixed some responsiveness on Contact Us screen

--- phitnest_mobile/lib/src/ui/screens/contactUs/contact_us_view.dart
+++ phitnest_mobile/lib/src/ui/screens/contactUs/contact_us_view.dart
@@ -23,77 +23,87 @@ class ContactUsView extends ScreenView {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: Column(children: [
-          80.verticalSpace,
-          SizedBox(
-              width: 301.w,
-              child: Text(
-                "How are we doing?",
-                style: Theme.of(context).textTheme.headlineLarge,
-                textAlign: TextAlign.center,
-              )),
-          40.verticalSpace,
-          SizedBox(
-              width: 301.w,
-              child: Text(
-                "Your input is invaluable in building stronger and healthier\ncommunities together.",
-                style: Theme.of(context).textTheme.labelLarge,
-                textAlign: TextAlign.center,
-              )),
-          30.verticalSpace,
-          SizedBox(
-              width: 291.w,
-              child: Form(
-                child: Column(children: [
+      body: SizedBox(
+          width: 1.sw,
+          height: 1.sh,
+          child: SingleChildScrollView(
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  62.verticalSpace,
                   SizedBox(
-                      height: 34.h,
-                      child: TextInputField(
-                          hint: 'Name', controller: nameController)),
-                  16.verticalSpace,
+                      width: 301.w,
+                      child: Text(
+                        "How are we doing?",
+                        style: Theme.of(context).textTheme.headlineLarge,
+                        textAlign: TextAlign.center,
+                      )),
+                  40.verticalSpace,
                   SizedBox(
-                      height: 34.h,
-                      child: TextInputField(
-                          hint: 'Email', controller: emailController)),
+                      width: 301.w,
+                      child: Text(
+                        "Your input is invaluable in building stronger and healthier\ncommunities together.",
+                        style: Theme.of(context).textTheme.labelLarge,
+                        textAlign: TextAlign.center,
+                      )),
+                  30.verticalSpace,
+                  SizedBox(
+                      width: 291.w,
+                      child: Form(
+                        child: Column(children: [
+                          SizedBox(
+                              height: 34.h,
+                              child: TextInputField(
+                                  hint: 'Name', controller: nameController)),
+                          16.verticalSpace,
+                          SizedBox(
+                              height: 34.h,
+                              child: TextInputField(
+                                  hint: 'Email', controller: emailController)),
+                          42.verticalSpace,
+                          SizedBox(
+                            height: 106.h,
+                            child: TextField(
+                                controller: feedbackController,
+                                textAlignVertical: TextAlignVertical.top,
+                                textAlign: TextAlign.left,
+                                expands: true,
+                                maxLines: null,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.all(16),
+                                    hintText: 'Your feedback',
+                                    hintStyle: Theme.of(context)
+                                        .textTheme
+                                        .labelMedium!
+                                        .copyWith(color: Color(0xFF999999)),
+                                    border: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Color(0xFF999999)),
+                                        borderRadius:
+                                            BorderRadius.circular(8)))),
+                          )
+                        ]),
+                      )),
+                  39.verticalSpace,
+                  StyledButton(
+                    child: Text('SUBMIT'),
+                    onPressed: onPressedSubmit,
+                  ),
                   42.verticalSpace,
-                  SizedBox(
-                    height: 106.h,
-                    child: TextField(
-                        controller: feedbackController,
-                        textAlignVertical: TextAlignVertical.top,
-                        textAlign: TextAlign.left,
-                        expands: true,
-                        maxLines: null,
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(16),
-                            hintText: 'Your feedback',
-                            hintStyle: Theme.of(context)
-                                .textTheme
-                                .labelMedium!
-                                .copyWith(color: Color(0xFF999999)),
-                            border: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Color(0xFF999999)),
-                                borderRadius: BorderRadius.circular(8)))),
-                  )
+                  TextButton(
+                      onPressed: onPressedExit,
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.transparent)),
+                      child: Text('EXIT',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall!
+                              .copyWith(
+                                  color: Colors.black,
+                                  fontStyle: FontStyle.italic,
+                                  decoration: TextDecoration.underline))),
+                  37.verticalSpace,
                 ]),
-              )),
-          39.verticalSpace,
-          StyledButton(
-            child: Text('SUBMIT'),
-            onPressed: onPressedSubmit,
-          ),
-          Expanded(child: Container()),
-          TextButton(
-              onPressed: onPressedExit,
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(Colors.transparent)),
-              child: Text('EXIT',
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                      color: Colors.black,
-                      fontStyle: FontStyle.italic,
-                      decoration: TextDecoration.underline))),
-          37.verticalSpace,
-        ]),
-      );
+          )));
 }
