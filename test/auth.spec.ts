df5eaaf4bb3eb2ff11a21a68dda7315f499e4ff1
Jp <jpsiegel8@gmail.com>
2022-10-18 22:26:28 -0400

    Fixed unit tests

---
+++ test/auth.spec.ts
@@ -0,0 +1,13 @@
+import "mocha";
+import request from "supertest";
+import Server from "../server";
+import { testUser, testUserPassword } from "./constants";
+
+describe("Register user that already exists", () => {
+  it("should not allow registration", () => request(Server).post("/auth/register").send({
+    email: testUser.email,
+    password: testUserPassword,
+    firstName: testUser.firstName,
+    lastName: testUser.lastName
+  }).expect(500));
+})
