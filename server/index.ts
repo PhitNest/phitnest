2f1d888ade92bea0c7a6361ecec065815849c0a2
Jp <jpsiegel8@gmail.com>
2022-11-01 16:51:18 -0400

    Fixed port

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ import Database from "./common/database";
 import Server from "./common/server";
 import routes from "./routes";
 
-const port = parseInt(process.env.PORT || "3000");
+const port = process.env.NODE_ENV == "production" ? 443 : parseInt(process.env.PORT || "3000");
 const connectionString =
   process.env.NODE_ENV === "production"
     ? process.env.MONGODB_URI
