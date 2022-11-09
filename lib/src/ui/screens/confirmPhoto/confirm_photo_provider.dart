11d4c766b84fefa6cadfaaedb3c4627979ae7ff5
rjaggannagari <rjaggannagari@vt.edu>
2022-11-09 17:02:03 -0500

    just made the folder for confirm email

---
+++ lib/src/ui/screens/confirmPhoto/confirm_photo_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../provider.dart';
+import 'confirm_photo_state.dart';
+import 'confirm_photo_view.dart';
+
+class ConfirmPhotoProvider extends ScreenProvider<ConfirmPhotoState, ConfirmPhotoView> {
+  @override
+  ConfirmPhotoView build(BuildContext context, ConfirmPhotoState state) => ConfirmPhotoView();
+
+  @override
+  ConfirmPhotoState buildState() => ConfirmPhotoState();
+}
---
+++ lib/src/ui/screens/confirmPhoto/confirm_photo_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+import '../view.dart';
+
+class ConfirmPhotoView extends ScreenView {
+  @override
+  Widget buildView(BuildContext context) => Container();
+}
