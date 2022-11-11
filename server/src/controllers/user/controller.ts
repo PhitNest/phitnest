831fd9b455f46cd38be6c4b7786eeb248a8cb04a
Jp <jpsiegel8@gmail.com>
2022-11-11 15:15:45 -0500

    Added a unit test for getting personal user data

--- server/src/controllers/user/controller.ts
+++ server/src/controllers/user/controller.ts
@@ -10,11 +10,23 @@ function publicDataResponse(user: IUserModel) {
   };
 }
 
+function privateDataResponse(user: IUserModel) {
+  return {
+    id: user._id,
+    cognitoId: user.cognitoId,
+    gymId: user.gymId,
+    firstName: user.firstName,
+    lastName: user.lastName,
+  };
+}
+
 class UserController {
   async myData(req: Request, res: Response) {
     return res
       .status(200)
-      .json(await UserQueries.getUser(res.locals.cognitoId));
+      .json(
+        privateDataResponse(await UserQueries.getUser(res.locals.cognitoId))
+      );
   }
 }
 
