9a620405e85d2843265f31761e0af0c28bd452fe
Jp <jpsiegel8@gmail.com>
2022-11-11 17:27:08 -0500

    Improved query performance

--- server/src/controllers/user/controller.ts
+++ server/src/controllers/user/controller.ts
@@ -2,30 +2,22 @@ import { Request, Response } from "express";
 import { UserQueries } from "../../queries/user.queries";
 import { IUserModel } from "../../models/user.model";
 
-function publicDataResponse(user: IUserModel) {
-  return {
-    id: user._id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-  };
-}
-
-function privateDataResponse(user: IUserModel) {
-  return {
-    id: user._id,
-    cognitoId: user.cognitoId,
-    gymId: user.gymId,
-    firstName: user.firstName,
-    lastName: user.lastName,
-  };
-}
-
 class UserController {
   async myData(req: Request, res: Response) {
     return res
       .status(200)
+      .json(await UserQueries.getPrivateUserData(res.locals.cognitoId));
+  }
+
+  async explore(req: Request, res: Response) {
+    return res
+      .status(200)
       .json(
-        privateDataResponse(await UserQueries.getUser(res.locals.cognitoId))
+        await UserQueries.explore(
+          res.locals.cognitoId,
+          parseInt(req.query.offset.toString()),
+          parseInt(req.query.limit.toString())
+        )
       );
   }
 }
