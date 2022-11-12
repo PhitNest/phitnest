266ab077b056f74a4e9e52aaefc7a149da6e40d5
rjaggannagari <rjaggannagari@vt.edu>
2022-11-12 11:59:56 -0500

    update

--- lib/src/ui/screens/confirmPhoto/confirm_photo_view.dart
+++ lib/src/ui/screens/confirmPhoto/confirm_photo_view.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../view.dart';
 
 class ConfirmPhotoView extends ScreenView {
   @override
-  Widget buildView(BuildContext context) => Container();
+  String? get appBarText => "Confirm Photo";
+
+  @override
+  Widget buildView(BuildContext context) => Container(
+            child: Image.asset(
+              'assets/images/phitnestSelfie.png',
+              height: 346.h,
+            ),
+          );
 }
