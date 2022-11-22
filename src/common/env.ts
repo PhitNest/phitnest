4491d225bec6cd7ed13fb498cc3452e13146b3e2
Jp <jpsiegel8@gmail.com>
2022-11-22 15:19:37 -0500

    Better environment variable usage

--- src/common/env.ts
+++ src/common/env.ts
@@ -1,5 +1,4 @@
 import dotenv from "dotenv";
-dotenv.config();
 
 export type Environment = {
   PORT: number;
@@ -8,15 +7,21 @@ export type Environment = {
   COGNITO_APP_CLIENT_ID: string;
 };
 
-export let env: Environment;
+let env: Environment;
 
-try {
-  env = {
-    PORT: parseInt(process.env.PORT!),
-    MONGODB_CONN_STRING: process.env.MONGODB_CONN_STRING!,
-    COGNITO_POOL_ID: process.env.COGNITO_POOL_ID!,
-    COGNITO_APP_CLIENT_ID: process.env.COGNITO_APP_CLIENT!,
-  };
-} catch (err) {
-  throw new Error("Could not parse environment variables.");
+export function getEnv(): Environment {
+  if (!env) {
+    dotenv.config();
+    try {
+      env = {
+        PORT: parseInt(process.env.PORT!),
+        MONGODB_CONN_STRING: process.env.MONGODB_CONN_STRING!,
+        COGNITO_POOL_ID: process.env.COGNITO_POOL_ID!,
+        COGNITO_APP_CLIENT_ID: process.env.COGNITO_APP_CLIENT_ID!,
+      };
+    } catch (err) {
+      throw new Error("Could not parse environment variables.");
+    }
+  }
+  return env;
 }
