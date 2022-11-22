ad76baca0b794713fd29e52bd5d1ddff9bca19e2
Jp <jpsiegel8@gmail.com>
2022-11-22 02:27:11 -0500

    Authentication repository implementation

--- src/adapters/repositories/implementations/user.repository.ts
+++ src/adapters/repositories/implementations/user.repository.ts
@@ -1,22 +1,30 @@
 import mongoose from "mongoose";
 import { IGymEntity, IUserEntity } from "../../../domain/entities";
 import { IUserRepository } from "../interfaces";
+import { GYM_COLLECTION_NAME, GYM_MODEL_NAME } from "./gym.repository";
+
+export const USER_COLLECTION_NAME = "users";
+export const USER_MODEL_NAME = "User";
 
 const schema = new mongoose.Schema(
   {
-    gymId: { type: mongoose.Types.ObjectId, ref: "Gym", required: true },
+    gymId: {
+      type: mongoose.Types.ObjectId,
+      ref: GYM_MODEL_NAME,
+      required: true,
+    },
     email: { type: String, required: true, unique: true, trim: true },
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
   },
   {
-    collection: "users",
+    collection: USER_COLLECTION_NAME,
   }
 );
 
 schema.index({ gymId: 1 });
 
-const User = mongoose.model<IUserEntity>("User", schema);
+const User = mongoose.model<IUserEntity>(USER_MODEL_NAME, schema);
 
 export class MongoUserRepository implements IUserRepository {
   async getGym(userId: string): Promise<IGymEntity> {
@@ -25,7 +33,7 @@ export class MongoUserRepository implements IUserRepository {
         { $match: { _id: userId } },
         {
           $lookup: {
-            from: "gyms",
+            from: GYM_COLLECTION_NAME,
             localField: "gymId",
             foreignField: "_id",
             as: "gym",
--- src/common/env.ts
+++ src/common/env.ts
@@ -1,2 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+export type Environment = {
+  PORT: number;
+  MONGODB_CONN_STRING: string;
+  COGNITO_POOL_ID: string;
+  COGNITO_APP_CLIENT_ID: string;
+};
+
+export let env: Environment;
+
+try {
+  env = {
+    PORT: parseInt(process.env.PORT!),
+    MONGODB_CONN_STRING: process.env.MONGODB_CONN_STRING!,
+    COGNITO_POOL_ID: process.env.COGNITO_POOL_ID!,
+    COGNITO_APP_CLIENT_ID: process.env.COGNITO_APP_CLIENT!,
+  };
+} catch (err) {
+  throw new Error("Could not parse environment variables.");
+}
