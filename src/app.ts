75d17b31c3b5feae84e6e42776b154d39a6a6e62
Jp <jpsiegel8@gmail.com>
2022-11-28 16:21:32 -0500

    Organized imports

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import { connect, disconnect } from "./common/database";
 import { injectDependencies, unbind } from "./common/dependency-injection";
-import { createServer, stopServer } from "./common/express/server";
+import { createServer, stopServer } from "./common/express";
 import dotenv from "dotenv";
 dotenv.config();
 
