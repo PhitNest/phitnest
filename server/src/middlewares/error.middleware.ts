f117135fc870532d023ce5efff0a4c0d9cd70445
Jp <jpsiegel8@gmail.com>
2022-11-30 18:14:51 -0500

    Cleared everything

--- server/src/middlewares/error.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export default function errorHandler(err, req: Request, res: Response) {
-  const errors = err.errors || [{ message: err.message }];
-  return res.status(err.status || 500).json({ errors });
-}
