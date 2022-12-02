8cf1e15e9cc568f4e641935a340a4950afb65cea
Jp <jpsiegel8@gmail.com>
2022-12-02 12:23:43 -0500

    Fixed build script

--- build.ts
+++ build.ts
@@ -5,7 +5,6 @@ const outDir = config.compilerOptions.outDir;
 
 s.rm("-rf", outDir);
 s.mkdir(outDir);
-s.cp(".env", `${outDir}/.env`);
 s.mkdir("-p", `${outDir}/openapi/swagger`);
 s.cp("openapi/api.yml", `${outDir}/openapi/api.yml`);
 s.mkdir("-p", `${outDir}/openapi/specs`);
