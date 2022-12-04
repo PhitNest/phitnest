1ab31d4ed455d14d247066e525977972b8cbcb30
XxThunderBlastxX <koustavmondal55@gmail.com>
2022-12-04 09:57:10 +0530

    Added options ui

--- lib/src/ui/screens/options/options_view.dart
+++ lib/src/ui/screens/options/options_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../view.dart';
 
@@ -6,8 +7,42 @@ class OptionsView extends NavBarScreenView {
   const OptionsView() : super();
 
   @override
-  Widget buildView(BuildContext context) => Container();
+  int get navbarIndex => 3;
 
   @override
-  int get navbarIndex => 3;
+  Widget buildView(BuildContext context) => Column(
+        children: [
+          Image.asset(
+            'assets/images/phitnestSelfie.png',
+            height: 280.h,
+            width: 1.sw,
+            fit: BoxFit.cover,
+          ),
+          40.verticalSpace,
+          Text(
+            'Eric-Michelle Jimenez',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          32.verticalSpace,
+          Text(
+            'erin@google.com',
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+          Divider(
+            thickness: 1,
+            indent: 32.w,
+            endIndent: 32.w,
+          ),
+          24.verticalSpace,
+          Text(
+            'Planet Fitness, Falls Church, VA',
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+          Divider(
+            thickness: 1,
+            indent: 32.w,
+            endIndent: 32.w,
+          ),
+        ],
+      );
 }
