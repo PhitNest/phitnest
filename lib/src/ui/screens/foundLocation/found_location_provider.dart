b205be27b739f1ca26c51b618296b54a7eb43eb0
Jp <jpsiegel8@gmail.com>
2023-01-11 09:55:54 -0500

    Added dispose method to state

--- lib/src/ui/screens/foundLocation/found_location_provider.dart
+++ lib/src/ui/screens/foundLocation/found_location_provider.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../../entities/entities.dart';
-import '../../../repositories/repositories.dart';
 import '../../widgets/widgets.dart';
 import '../screen_provider.dart';
 import '../screens.dart';
