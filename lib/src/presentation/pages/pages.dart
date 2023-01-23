f0bd8069f71912d7523dbfbb2167ba3e971dcbfd
Jp <jpsiegel8@gmail.com>
2023-01-23 15:09:28 -0500

    Refactor

--- lib/src/presentation/pages/pages.dart
+++ lib/src/presentation/pages/pages.dart
@@ -3,4 +3,6 @@ export 'forgot_password/ui/forgot_password.dart';
 export 'home/home.dart';
 export 'login/ui/login_page.dart';
 export 'on_boarding/on_boarding.dart';
-export 'registration/ui/registration_page.dart';
+export 'forgot_password/forgot_password.dart';
+export 'confirm_email/ui/confirm_email_page.dart';
+export 'home/home.dart';
