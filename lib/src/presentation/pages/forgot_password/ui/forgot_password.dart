2b251999478c29567c85102d0e1422e91134e891
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-01-26 01:25:38 +0530

    Refactor forgotPassword page

--- lib/src/presentation/pages/forgot_password/ui/forgot_password.dart
+++ lib/src/presentation/pages/forgot_password/ui/forgot_password.dart
@@ -15,49 +15,46 @@ class ForgotPasswordPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ForgotPasswordBloc>(
-      create: (context) => ForgotPasswordBloc(),
-      child: BlocConsumer(
-        listener: (context, state) {
-          if (state is ForgotPasswordSuccessState) {
-            Navigator.of(context).pushAndRemoveUntil(
-              CupertinoPageRoute(
-                builder: (context) => LoginPage(),
-              ),
-              (_) => false,
-            );
-          }
-        },
-        builder: (context, state) {
-          if (state is ForgotPasswordLoadingState) {
-            return ForgotPasswordLoadingPage(
-              emailController: state.emailController,
-              passwordController: state.passwordController,
-              confirmPassController: state.confirmPassController,
-              emailFocusNode: state.emailFocusNode,
-              passwordFocusNode: state.passwordFocusNode,
-              confirmPassFocusNode: state.confirmPassFocusNode,
-              onSubmit: () => context
-                  .read<ForgotPasswordBloc>()
-                  .add(ForgotPasswordOnSubmitEvent()),
-            );
-          } else if (state is ForgotPasswordInitialState) {
-            return ForgotPasswordInitialPage(
-              emailController: state.emailController,
-              passwordController: state.passwordController,
-              confirmPassController: state.confirmPassController,
-              emailFocusNode: state.emailFocusNode,
-              passwordFocusNode: state.passwordFocusNode,
-              confirmPassFocusNode: state.confirmPassFocusNode,
-              onSubmit: () => context
-                  .read<ForgotPasswordBloc>()
-                  .add(ForgotPasswordOnSubmitEvent()),
-            );
-          } else {
-            throw Exception('Invalid state: $state');
-          }
-        },
-      ),
+    return BlocConsumer<ForgotPasswordBloc, ForgotPasswordState>(
+      listener: (context, state) {
+        if (state is ForgotPasswordSuccessState) {
+          Navigator.of(context).pushAndRemoveUntil(
+            CupertinoPageRoute(
+              builder: (context) => LoginPage(),
+            ),
+            (_) => false,
+          );
+        }
+      },
+      builder: (context, state) {
+        if (state is ForgotPasswordLoadingState) {
+          return ForgotPasswordLoadingPage(
+            emailController: state.emailController,
+            passwordController: state.passwordController,
+            confirmPassController: state.confirmPassController,
+            emailFocusNode: state.emailFocusNode,
+            passwordFocusNode: state.passwordFocusNode,
+            confirmPassFocusNode: state.confirmPassFocusNode,
+            onSubmit: () => context
+                .read<ForgotPasswordBloc>()
+                .add(ForgotPasswordOnSubmitEvent()),
+          );
+        } else if (state is ForgotPasswordInitialState) {
+          return ForgotPasswordInitialPage(
+            emailController: state.emailController,
+            passwordController: state.passwordController,
+            confirmPassController: state.confirmPassController,
+            emailFocusNode: state.emailFocusNode,
+            passwordFocusNode: state.passwordFocusNode,
+            confirmPassFocusNode: state.confirmPassFocusNode,
+            onSubmit: () => context
+                .read<ForgotPasswordBloc>()
+                .add(ForgotPasswordOnSubmitEvent()),
+          );
+        } else {
+          throw Exception('Invalid state: $state');
+        }
+      },
     );
   }
 }
