b68a2b88faa9022782202f7a538cdd3fcf31a7e4
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-01-26 21:34:17 +0530

    Routed verifcation code page for forgotPasswordPage

--- lib/src/presentation/pages/forgot_password/state/success.dart
+++ lib/src/presentation/pages/forgot_password/state/success.dart
@@ -1,8 +1,11 @@
 import '../bloc/forgot_password_bloc.dart';
 
 class ForgotPasswordSuccessState extends ForgotPasswordState {
+  final String email;
 
-  const ForgotPasswordSuccessState() : super();
+  const ForgotPasswordSuccessState({
+    required this.email,
+  }) : super();
 
   @override
   List<Object?> get props => [];
