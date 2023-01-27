75ffa2f762887cb03b4d40773db75f2610dc4ed6
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-01-27 20:28:01 +0530

    Added all the routes

--- lib/src/common/constants/backend_routes.dart
+++ lib/src/common/constants/backend_routes.dart
@@ -22,6 +22,17 @@ enum Routes {
   getUploadUrlUnauthorized,
   nearestGyms,
   resendConfirmationCode,
+  signOut,
+  refreshSession,
+  explore,
+  sendFriendRequest,
+  denyFriendRequest,
+  removeFriend,
+  friendsAndRequests,
+  friendsAndMessages,
+  getDirectMessages,
+  unauthorizedUploadPictureUpload,
+  profilePictureUpload,
 }
 
 extension RouteInstance on Routes {
@@ -45,6 +56,28 @@ extension RouteInstance on Routes {
         return const Route('/gym/nearest', HttpMethod.get);
       case Routes.resendConfirmationCode:
         return const Route('/auth/resendConfirmation', HttpMethod.post);
+      case Routes.signOut:
+        return const Route('/auth/signOut', HttpMethod.post);
+      case Routes.refreshSession:
+        return const Route('/auth/refreshSession', HttpMethod.post);
+      case Routes.explore:
+        return const Route('/user/explore', HttpMethod.get);
+      case Routes.sendFriendRequest:
+        return const Route('/friendRequest', HttpMethod.post);
+      case Routes.denyFriendRequest:
+        return const Route('/friendRequest/deny', HttpMethod.post);
+      case Routes.removeFriend:
+        return const Route('/friendship', HttpMethod.delete);
+      case Routes.friendsAndRequests:
+        return const Route('/friendship/friendsAndRequests', HttpMethod.get);
+      case Routes.friendsAndMessages:
+        return const Route('/friendship/friendsAndMessages', HttpMethod.get);
+      case Routes.getDirectMessages:
+        return const Route('/directMessage/list', HttpMethod.get);
+      case Routes.unauthorizedUploadPictureUpload:
+        return const Route('/profilePicture/unauthorized', HttpMethod.get);
+      case Routes.profilePictureUpload:
+        return const Route('/profilePicture/upload', HttpMethod.get);
     }
   }
 }
