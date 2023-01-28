94a51adcdeda213e25a547d7e571acf6e9cb21cb
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-01-28 18:16:54 +0530

    Refactor of code

--- lib/src/domain/entities/entities.dart
+++ lib/src/domain/entities/entities.dart
@@ -1,5 +1,7 @@
 export 'address.entity.dart';
-export 'location.entity.dart';
-export 'gym.entity.dart';
 export 'auth.entity.dart';
+export 'friendRequest.entity.dart';
+export 'friendship.entity.dart';
+export 'gym.entity.dart';
+export 'location.entity.dart';
 export 'user.entity.dart';
