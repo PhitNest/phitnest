d3ac9e16837f990c83c2c06a8a2631b2ef9e9ce8
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-02-01 20:16:57 +0530

    Encapsulated error banner

---
+++ lib/src/presentation/widgets/styled/styled_error_banner.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../../common/theme.dart';
+
+class StyledErrorBanner extends MaterialBanner {
+  final String err;
+  final BuildContext context;
+
+  StyledErrorBanner({
+    required this.err,
+    required this.context,
+  }) : super(
+          content: Text(
+            err,
+            style: theme.textTheme.bodySmall!.copyWith(
+              color: theme.errorColor,
+            ),
+          ),
+          padding: EdgeInsets.all(10),
+          elevation: 8,
+          onVisible: () => Future.delayed(Duration(seconds: 3)).then(
+            (_) => ScaffoldMessenger.of(context).hideCurrentMaterialBanner(),
+          ),
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.error_outline,
+            color: theme.errorColor,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentMaterialBanner();
+              },
+              child: Text(
+                'Dismiss',
+                style: theme.textTheme.bodySmall,
+              ),
+            )
+          ],
+        );
+}
