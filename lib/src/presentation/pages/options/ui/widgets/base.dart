b934e1bb7bad4523cab99f4c55f3aa9198345229
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-02-08 00:58:47 +0530

    Fixed options screen

--- lib/src/presentation/pages/options/ui/widgets/base.dart
+++ lib/src/presentation/pages/options/ui/widgets/base.dart
@@ -16,53 +16,66 @@ abstract class _BasePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => StyledScaffold(
-        body: Column(
-          children: [
-            Stack(
-              children: [
-                Image.network(user.profilePictureUrl),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 32.h,
-                    right: 24.w,
-                  ),
-                  child: Positioned(
-                    top: 0,
-                    right: 0,
-                    child: IconButton(
-                      icon: Icon(Icons.more_horiz_sharp),
-                      onPressed: onEditProfilePicture,
-                      color: Color(0xFFC11C1C),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Image.network(user.profilePictureUrl),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 32.h,
+                      right: 24.w,
+                    ),
+                    child: Positioned(
+                      top: 0,
+                      right: 0,
+                      child: IconButton(
+                        icon: Icon(Icons.more_horiz_sharp),
+                        onPressed: onEditProfilePicture,
+                        color: Color(0xFFC11C1C),
+                      ),
                     ),
                   ),
+                ],
+              ),
+              40.verticalSpace,
+              SizedBox(
+                width: 291.w,
+                child: Column(
+                  children: [
+                    Text(
+                      user.fullName,
+                      style: theme.textTheme.headlineLarge,
+                    ),
+                    // 32.verticalSpace,
+                    Text(
+                      user.email,
+                      style: theme.textTheme.labelMedium,
+                    ),
+                    Divider(
+                      thickness: 1.0,
+                    ),
+                    12.verticalSpace,
+                    Text(
+                      'Address',
+                      style: theme.textTheme.headlineLarge,
+                    ),
+                    // 32.verticalSpace,
+                    Text(
+                      gym.toString(),
+                      style: theme.textTheme.labelMedium,
+                    ),
+                    Divider(
+                      thickness: 1.0,
+                    ),
+                    45.verticalSpace,
+                    child,
+                  ],
                 ),
-              ],
-            ),
-            40.verticalSpace,
-            Text(
-              user.fullName,
-              style: theme.textTheme.headlineLarge,
-            ),
-            32.verticalSpace,
-            Text(
-              user.email,
-              style: theme.textTheme.labelLarge,
-            ),
-            Divider(),
-            24.verticalSpace,
-            Text(
-              'Address',
-              style: theme.textTheme.headlineLarge,
-            ),
-            32.verticalSpace,
-            Text(
-              gym.toString(),
-              style: theme.textTheme.labelLarge,
-            ),
-            Divider(),
-            45.verticalSpace,
-            child,
-          ],
+              )
+            ],
+          ),
         ),
       );
 }
