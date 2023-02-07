2e7cc97fdc17273341963b3f66f9253b321183b1
Jp <jpsiegel8@gmail.com>
2023-02-07 13:04:35 -0500

    Made the unauthorized profile picture route unsecure. This is a risk but it is made so users cannot get into invalid states by losing their password. That would potentially prohibit a users email from being registered.

--- src/domain/use-cases/unauthorized-upload-url.ts
+++ src/domain/use-cases/unauthorized-upload-url.ts
@@ -1,36 +1,23 @@
-import {
-  kUserAlreadyConfirmed,
-  kUserNotConfirmed,
-} from "../../common/failures";
+import { kUserAlreadyConfirmed } from "../../common/failures";
 import { Failure } from "../../common/types";
-import { authRepo, userRepo, profilePictureRepo } from "../repositories";
+import { userRepo, profilePictureRepo } from "../repositories";
 
-export async function unauthorizedProfilePictureUploadUrl(
-  email: string,
-  password: string
-) {
-  const result = await authRepo.login(email, password);
-  if (result instanceof Failure) {
-    if (result.code === kUserNotConfirmed.code) {
-      const user = await userRepo.getByEmail(email);
-      if (user instanceof Failure) {
-        return user;
-      } else {
-        const url = await profilePictureRepo.getProfilePictureUploadUrl(
-          user.cognitoId
-        );
-        if (url instanceof Failure) {
-          return url;
-        } else {
-          return {
-            url: url,
-          };
-        }
-      }
-    } else {
-      return result;
-    }
-  } else {
+export async function unauthorizedProfilePictureUploadUrl(email: string) {
+  const user = await userRepo.getByEmail(email);
+  if (user instanceof Failure) {
+    return user;
+  } else if (user.confirmed) {
     return kUserAlreadyConfirmed;
+  } else {
+    const url = await profilePictureRepo.getProfilePictureUploadUrl(
+      user.cognitoId
+    );
+    if (url instanceof Failure) {
+      return url;
+    } else {
+      return {
+        url: url,
+      };
+    }
   }
 }
