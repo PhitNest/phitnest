98689aa7c196ce38c719733e27aadd7fa6ade6cc
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-02-19 19:21:35 +0530

    Login error fix

--- lib/src/presentation/pages/login/login.dart
+++ lib/src/presentation/pages/login/login.dart
@@ -8,6 +8,7 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -23,22 +24,19 @@ import '../pages.dart';
 part 'bloc/login_bloc.dart';
 part 'bloc/on_cancel.dart';
 part 'bloc/on_error.dart';
-part 'bloc/on_success.dart';
 part 'bloc/on_submit.dart';
-
-part 'event/cancel.dart';
+part 'bloc/on_success.dart';
 part 'event/base.dart';
+part 'event/cancel.dart';
+part 'event/error.dart';
 part 'event/submit.dart';
 part 'event/success.dart';
-part 'event/error.dart';
-
 part 'state/base.dart';
-part 'state/success.dart';
-part 'state/initial.dart';
-part 'state/loading.dart';
 part 'state/confirming_email.dart';
 part 'state/error.dart';
-
+part 'state/initial.dart';
+part 'state/loading.dart';
+part 'state/success.dart';
 part 'ui/login_page.dart';
 part 'ui/widgets/base.dart';
 part 'ui/widgets/initial.dart';
