47070bc096efa9ebf9f76d314d1de19c0d3abdb4
Jp <jpsiegel8@gmail.com>
2023-02-19 14:09:17 -0500

    Fixed scrollable view

--- lib/src/presentation/pages/chat/ui/widgets/available_chat.dart
+++ lib/src/presentation/pages/chat/ui/widgets/available_chat.dart
@@ -8,34 +8,32 @@ class _AvailableChatPage extends _IChatPageBase {
     required this.messages,
   }) : super(
           key: key,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 28.w),
-            child: Expanded(child: Container()),
-            // child: Expanded(
-            //   // height: messages.length * 50.h * 50,
-            //   child: ListView.builder(
-            //     itemCount: messages.length * 50,
-            //     itemBuilder: (context, index) {
-            //       return Column(
-            //         children: [
-            //           ListTile(
-            //             title: Text(
-            //               '${messages[index % messages.length].friendship.friend.firstName} ${messages[index % messages.length].friendship.friend.lastName}',
-            //               style: theme.textTheme.headlineSmall,
-            //             ),
-            //             subtitle: Text(
-            //               messages[index % messages.length].message!.text,
-            //               style: theme.textTheme.bodyMedium!.copyWith(
-            //                 color: Color(0xFF6B6B6B),
-            //               ),
-            //             ),
-            //           ),
-            //           40.verticalSpace,
-            //         ],
-            //       );
-            //     },
-            //   ),
-            // ),
+          child: Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 28.w),
+              child: ListView.builder(
+                itemCount: messages.length * 50,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        title: Text(
+                          '${messages[index % messages.length].friendship.friend.firstName} ${messages[index % messages.length].friendship.friend.lastName}',
+                          style: theme.textTheme.headlineSmall,
+                        ),
+                        subtitle: Text(
+                          messages[index % messages.length].message!.text,
+                          style: theme.textTheme.bodyMedium!.copyWith(
+                            color: Color(0xFF6B6B6B),
+                          ),
+                        ),
+                      ),
+                      40.verticalSpace,
+                    ],
+                  );
+                },
+              ),
+            ),
           ),
         );
 }
