9bfcb68dc8637def792d397413efa6322d3937ec
Jp <jpsiegel8@gmail.com>
2023-02-21 11:57:34 -0500

    Refactored caching

--- lib/src/common/secure_storage.dart
+++ lib/src/common/secure_storage.dart
@@ -9,21 +9,21 @@ final _secureStorage = FlutterSecureStorage(
   iOptions: IOSOptions(accessibility: KeychainAccessibility.first_unlock),
 );
 
-late Map<String, String> _cache;
+late Map<String, String> _stringifiedCache;
 Map<String, Serializable> _lazyLoadedCache = {};
 Map<String, List<Serializable>> _lazyLoadedListCache = {};
 
 Future<void> loadSecureStorage() async {
-  _cache = await _secureStorage.readAll();
+  _stringifiedCache = await _secureStorage.readAll();
 }
 
 Future<void> cacheObject<T extends Serializable>(String key, T? value) async {
   if (value != null) {
-    _cache[key] = jsonEncode(value.toJson());
-    await _secureStorage.write(key: key, value: _cache[key]);
+    _stringifiedCache[key] = jsonEncode(value.toJson());
+    await _secureStorage.write(key: key, value: _stringifiedCache[key]);
   } else {
     _lazyLoadedCache.remove(key);
-    _cache.remove(key);
+    _stringifiedCache.remove(key);
     await _secureStorage.delete(key: key);
   }
 }
@@ -32,11 +32,11 @@ T? getCachedObject<T extends Serializable>(
   String key,
   T Function(Map<String, dynamic>) parseJson,
 ) {
-  String? encoded = _cache[key];
   if (_lazyLoadedCache[key] != null) {
     return _lazyLoadedCache[key] as T;
-  } else if (encoded != null) {
-    return _lazyLoadedCache[key] = parseJson(jsonDecode(encoded));
+  } else if (_stringifiedCache[key] != null) {
+    return _lazyLoadedCache[key] =
+        parseJson(jsonDecode(_stringifiedCache[key]!));
   } else {
     return null;
   }
@@ -46,14 +46,14 @@ List<T>? getCachedList<T extends Serializable>(
   String key,
   T Function(Map<String, dynamic>) parseJson,
 ) {
-  String? encoded = _cache[key];
   if (_lazyLoadedListCache[key] != null) {
     return _lazyLoadedListCache[key] as List<T>;
-  } else if (encoded != null) {
-    return _lazyLoadedListCache[key] = (jsonDecode(encoded) as List)
-        .map((e) => parseJson(e))
-        .toList()
-        .cast<T>();
+  } else if (_stringifiedCache[key] != null) {
+    return _lazyLoadedListCache[key] =
+        (jsonDecode(_stringifiedCache[key]!) as List)
+            .map((e) => parseJson(e))
+            .toList()
+            .cast<T>();
   } else {
     return null;
   }
@@ -64,22 +64,22 @@ Future<void> cacheList<T extends Serializable>(
   List<T>? value,
 ) async {
   if (value != null) {
-    _cache[key] = jsonEncode(value.map((e) => e.toJson()).toList());
-    await _secureStorage.write(key: key, value: _cache[key]);
+    _stringifiedCache[key] = jsonEncode(value.map((e) => e.toJson()).toList());
+    await _secureStorage.write(key: key, value: _stringifiedCache[key]);
   } else {
     _lazyLoadedListCache.remove(key);
-    _cache.remove(key);
+    _stringifiedCache.remove(key);
     await _secureStorage.delete(key: key);
   }
 }
 
 Future<void> cacheString(String key, String? value) async {
   if (value != null) {
-    _cache[key] = value;
+    _stringifiedCache[key] = value;
   } else {
-    _cache.remove(key);
+    _stringifiedCache.remove(key);
   }
   await _secureStorage.write(key: key, value: value);
 }
 
-String? getCachedString(String key) => _cache[key];
+String? getCachedString(String key) => _stringifiedCache[key];
