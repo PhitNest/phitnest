c9a5e764eb030d8aaccb565274f37804f8a5f06d
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-02-23 01:18:20 +0530

    Implemented ui of friends screen

---
+++ lib/src/presentation/pages/friends/ui/friends_page.dart
@@ -0,0 +1,10 @@
+part of friends;
+
+class FriendsPage extends StatelessWidget {
+  const FriendsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const IBaseFriendsPage();
+  }
+}
--- lib/src/presentation/pages/home/home.dart
+++ lib/src/presentation/pages/home/home.dart
@@ -21,37 +21,58 @@ import '../../../domain/entities/entities.dart';
 import '../../../domain/repositories/repository.dart';
 import '../../../domain/use_cases/use_cases.dart';
 import '../../widgets/widgets.dart';
+import '../friends/friends.dart';
 import '../pages.dart';
 
-part 'ui/home_page.dart';
-
-part 'ui/explore/explore_page.dart';
-part 'ui/explore/widgets/base.dart';
-part 'ui/explore/widgets/loading.dart';
-part 'ui/explore/widgets/card.dart';
-part 'ui/explore/widgets/empty.dart';
-
-part 'ui/options/options_page.dart';
-part 'ui/options/widgets/base.dart';
-part 'ui/options/widgets/loaded.dart';
-part 'ui/options/widgets/loading.dart';
-part 'ui/options/widgets/sign_out_loading.dart';
-
-part 'ui/chat/chat_page.dart';
-part 'ui/chat/widgets/base.dart';
-part 'ui/chat/widgets/loading.dart';
-part 'ui/chat/widgets/empty.dart';
-part 'ui/chat/widgets/available_chat.dart';
-
-part 'state/home/base.dart';
-part 'state/home/initial.dart';
-part 'state/home/socket_connected.dart';
-
+part 'bloc/chat/chat_bloc.dart';
+part 'bloc/chat/on_loaded.dart';
+part 'bloc/chat/on_loading_error.dart';
+part 'bloc/explore/explore_bloc.dart';
+part 'bloc/explore/on_increment_countdown.dart';
+part 'bloc/explore/on_loaded.dart';
+part 'bloc/explore/on_loading_error.dart';
+part 'bloc/explore/on_press_down.dart';
+part 'bloc/explore/on_release.dart';
+part 'bloc/home/home_bloc.dart';
+part 'bloc/home/on_refresh_session.dart';
+part 'bloc/home/on_set_page.dart';
+part 'bloc/home/on_sign_out.dart';
+part 'bloc/home/on_socket_connect.dart';
+part 'bloc/home/on_socket_connect_error.dart';
+part 'bloc/options/on_edit_profile_picture.dart';
+part 'bloc/options/on_error.dart';
+part 'bloc/options/on_loaded.dart';
+part 'bloc/options/on_set_profile_picture.dart';
+part 'bloc/options/on_sign_out.dart';
+part 'bloc/options/on_sign_out_response.dart';
+part 'bloc/options/options_bloc.dart';
+part 'event/chat/base.dart';
+part 'event/chat/loaded.dart';
+part 'event/chat/loading_error.dart';
+part 'event/explore/base.dart';
+part 'event/explore/increment_countdown.dart';
+part 'event/explore/loaded.dart';
+part 'event/explore/loading_error.dart';
+part 'event/explore/press_down.dart';
+part 'event/explore/release.dart';
+part 'event/explore/send_friend_request.dart';
+part 'event/home/base.dart';
+part 'event/home/refresh_session.dart';
+part 'event/home/set_page.dart';
+part 'event/home/sign_out.dart';
+part 'event/home/socket_connect_error.dart';
+part 'event/home/socket_connected.dart';
+part 'event/options/base.dart';
+part 'event/options/edit_profile_picture.dart';
+part 'event/options/error.dart';
+part 'event/options/loaded.dart';
+part 'event/options/set_profile_picture.dart';
+part 'event/options/sign_out.dart';
+part 'event/options/sign_out_response.dart';
 part 'state/chat/base.dart';
 part 'state/chat/loaded.dart';
 part 'state/chat/loading.dart';
 part 'state/chat/reloading.dart';
-
 part 'state/explore/base.dart';
 part 'state/explore/holding.dart';
 part 'state/explore/loaded.dart';
@@ -59,65 +80,29 @@ part 'state/explore/loading.dart';
 part 'state/explore/matched.dart';
 part 'state/explore/reloading.dart';
 part 'state/explore/sending.dart';
-
+part 'state/home/base.dart';
+part 'state/home/initial.dart';
 part 'state/home/sign_out.dart';
-
+part 'state/home/socket_connected.dart';
 part 'state/options/base.dart';
-part 'state/options/loaded.dart';
 part 'state/options/edit_profile_picture.dart';
 part 'state/options/initial.dart';
+part 'state/options/loaded.dart';
 part 'state/options/sign_out.dart';
 part 'state/options/sign_out_loading.dart';
-
-part 'bloc/home/home_bloc.dart';
-part 'bloc/home/on_refresh_session.dart';
-part 'bloc/home/on_set_page.dart';
-part 'bloc/home/on_socket_connect_error.dart';
-part 'bloc/home/on_socket_connect.dart';
-part 'bloc/home/on_sign_out.dart';
-
-part 'bloc/options/options_bloc.dart';
-part 'bloc/options/on_sign_out.dart';
-part 'bloc/options/on_error.dart';
-part 'bloc/options/on_edit_profile_picture.dart';
-part 'bloc/options/on_loaded.dart';
-part 'bloc/options/on_sign_out_response.dart';
-part 'bloc/options/on_set_profile_picture.dart';
-
-part 'bloc/explore/explore_bloc.dart';
-part 'bloc/explore/on_press_down.dart';
-part 'bloc/explore/on_release.dart';
-part 'bloc/explore/on_increment_countdown.dart';
-part 'bloc/explore/on_loaded.dart';
-part 'bloc/explore/on_loading_error.dart';
-
-part 'bloc/chat/chat_bloc.dart';
-part 'bloc/chat/on_loaded.dart';
-part 'bloc/chat/on_loading_error.dart';
-
-part 'event/home/base.dart';
-part 'event/home/refresh_session.dart';
-part 'event/home/set_page.dart';
-part 'event/home/socket_connect_error.dart';
-part 'event/home/socket_connected.dart';
-part 'event/home/sign_out.dart';
-
-part 'event/explore/base.dart';
-part 'event/explore/press_down.dart';
-part 'event/explore/release.dart';
-part 'event/explore/increment_countdown.dart';
-part 'event/explore/loaded.dart';
-part 'event/explore/loading_error.dart';
-part 'event/explore/send_friend_request.dart';
-
-part 'event/chat/base.dart';
-part 'event/chat/loaded.dart';
-part 'event/chat/loading_error.dart';
-
-part 'event/options/base.dart';
-part 'event/options/sign_out.dart';
-part 'event/options/edit_profile_picture.dart';
-part 'event/options/loaded.dart';
-part 'event/options/sign_out_response.dart';
-part 'event/options/error.dart';
-part 'event/options/set_profile_picture.dart';
+part 'ui/chat/chat_page.dart';
+part 'ui/chat/widgets/available_chat.dart';
+part 'ui/chat/widgets/base.dart';
+part 'ui/chat/widgets/empty.dart';
+part 'ui/chat/widgets/loading.dart';
+part 'ui/explore/explore_page.dart';
+part 'ui/explore/widgets/base.dart';
+part 'ui/explore/widgets/card.dart';
+part 'ui/explore/widgets/empty.dart';
+part 'ui/explore/widgets/loading.dart';
+part 'ui/home_page.dart';
+part 'ui/options/options_page.dart';
+part 'ui/options/widgets/base.dart';
+part 'ui/options/widgets/loaded.dart';
+part 'ui/options/widgets/loading.dart';
+part 'ui/options/widgets/sign_out_loading.dart';
