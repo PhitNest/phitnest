17947a6708afcb6bc81ed0e830b01d054a3634fb
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-02-25 23:40:34 +0530

    Added screen to message page

--- lib/src/presentation/pages/message/ui/message_page.dart
+++ lib/src/presentation/pages/message/ui/message_page.dart
@@ -15,7 +15,35 @@ class MessagePage extends StatelessWidget {
         value: homeBloc,
         child: BlocConsumer<HomeBloc, IHomeState>(
           listener: (context, state) {},
-          builder: (context, state) => ,
+          builder: (context, homeState) => BlocWidget(
+            create: (context) => _MessageBloc(
+              authMethods: context.authMethods,
+              friendship: friendship,
+            ),
+            builder: (context, state) {
+              if (state is _LoadedState) {
+                if (Cache.directMessage
+                        .getDirectMessages(friendship.friend.id) !=
+                    null) {
+                  return _MessageLoaded(
+                    message: 'message',
+                    sentByMe: false,
+                    name: 'name',
+                  );
+                } else {
+                  return _EmptyMessagePage(
+                    name: 'name',
+                  );
+                }
+              } else if (state is _LoadingState) {
+                return _LoadingPage(
+                  name: 'name',
+                );
+              } else {
+                return _LoadingPage(name: 'name');
+              }
+            },
+          ),
         ),
       );
 }
