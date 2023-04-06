d51919496ee79de5385854b08c2d0916bffde0b1
Jp <jpsiegel8@gmail.com>
2023-04-06 12:07:49 -0400

    Added phitnest core

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'PhitNest Admin',
-    theme: ThemeData(
-      primarySwatch: Colors.grey,
-      useMaterial3: true,
-    ),
-    home: Scaffold(
-      body: Container(
-        child: Text('PhitNest Admin'),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PhitNest Admin',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Container(
+          child: Text('PhitNest Admin'),
+        ),
       ),
     ),
-  ));
+  );
 }
