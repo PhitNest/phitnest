32d8a58b5ca4028dc1f280ec49400cc33691f092
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-04-09 13:08:57 +0530

    Implemented login ui

---
+++ lib/common/widgets/styled_text_field.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+import '../../theme.dart';
+
+class StyledTextField extends StatelessWidget {
+  final String? hint;
+  final String? error;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final FocusNode? focusNode;
+  final VoidCallback? onEditingComplete;
+  final ValueChanged<String>? onChanged;
+  final double? width;
+  final double? height;
+  final TextCapitalization textCapitalization;
+  final int? maxLines;
+  final ValueChanged<String>? onFieldSubmitted;
+  final int? errorMaxLines;
+
+  const StyledTextField({
+    Key? key,
+    this.hint,
+    this.error,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType,
+    this.maxLines,
+    this.textInputAction,
+    this.focusNode,
+    this.onEditingComplete,
+    this.onChanged,
+    this.width,
+    this.onFieldSubmitted,
+    this.height,
+    this.textCapitalization = TextCapitalization.none,
+    this.errorMaxLines,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        width: width,
+        height: height,
+        child: TextFormField(
+          controller: controller,
+          obscureText: obscureText,
+          keyboardType: keyboardType,
+          textInputAction: textInputAction,
+          onFieldSubmitted: onFieldSubmitted,
+          focusNode: focusNode,
+          maxLines: maxLines,
+          onEditingComplete: onEditingComplete,
+          onChanged: onChanged,
+          style: theme.textTheme.labelMedium,
+          textCapitalization: textCapitalization,
+          decoration: InputDecoration(
+            fillColor: Colors.white,
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 24, horizontal: 32),
+            helperText: ' ',
+            hintText: hint,
+            errorText: error,
+            errorMaxLines: errorMaxLines,
+            helperStyle: theme.textTheme.labelMedium,
+            errorStyle:
+                theme.textTheme.labelMedium!.copyWith(color: Colors.red),
+            hintStyle: theme.textTheme.labelMedium,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:phitnest_web_admin/router.dart';
+import 'package:phitnest_web_admin/theme.dart';
 
 void main() {
+  usePathUrlStrategy();
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'PhitNest Admin',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        body: Container(
-          child: Text('PhitNest Admin'),
-        ),
-      ),
+    const ProviderScope(
+      child: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'PhitNest Admin',
+      theme: theme,
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: appRouter.router.routeInformationParser,
+      routerDelegate: appRouter.router.routerDelegate,
+    );
+  }
 }
