303d74803c60e3b08cd6f6007821a38909e6d4f8
Jp <jpsiegel8@gmail.com>
2023-05-02 10:35:19 -0400

    Initial commit

--- aws/scripts/install_dgraph.sh
+++
@@ -1,2 +0,0 @@
-sudo yum update -y ca-certificates
-curl https://get.dgraph.io -sSf | bash
--- src/lambdas/gym/nearest/get.test.ts
+++
@@ -1,5 +0,0 @@
-describe("get nearest gyms", () => {
-  it("should return a list of gyms", async () => {
-    expect(true).toBeTruthy();
-  });
-});
