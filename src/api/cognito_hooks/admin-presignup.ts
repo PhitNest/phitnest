dbc39ea7f423576502d3b63b9286052d44dcec85
Jp <jpsiegel8@gmail.com>
2023-05-19 21:42:06 -0400

    Fixed admin presignup hook

--- src/api/cognito_hooks/admin-presignup.ts
+++ src/api/cognito_hooks/admin-presignup.ts
@@ -4,27 +4,20 @@ import { PreSignUpTriggerEvent } from "aws-lambda";
 
 export async function invoke(event: PreSignUpTriggerEvent) {
   const client = connectDynamo();
-  const { firstName, lastName } = event.request.userAttributes;
-  if (firstName && lastName) {
-    await client.send(
-      new TransactWriteItemsCommand({
-        TransactItems: [
-          {
-            Put: {
-              TableName: process.env.DYNAMO_TABLE_NAME,
-              Item: {
-                part_id: { S: "ADMINS" },
-                sort_id: { S: `ADMIN#${event.userName}` },
-                firstName: { S: event.request.userAttributes.firstName },
-                lastName: { S: event.request.userAttributes.lastName },
-                email: { S: event.request.userAttributes.email },
-              },
+  await client.send(
+    new TransactWriteItemsCommand({
+      TransactItems: [
+        {
+          Put: {
+            TableName: process.env.DYNAMO_TABLE_NAME,
+            Item: {
+              part_id: { S: "ADMINS" },
+              sort_id: { S: `ADMIN#${event.userName}` },
+              email: { S: event.request.userAttributes.email },
             },
           },
-        ],
-      })
-    );
-  } else {
-    throw new Error("Missing required attributes");
-  }
+        },
+      ],
+    })
+  );
 }
