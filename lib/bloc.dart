0393b31393622d2605e6564fe8315bff9a65d37f
Jp <jpsiegel8@gmail.com>
2023-05-21 21:16:35 -0400

    Removed flutter

--- lib/bloc.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-extension BlocGetter on BuildContext {
-  T bloc<T extends Bloc<Event, State>, State, Event>() => BlocProvider.of(this);
-}
