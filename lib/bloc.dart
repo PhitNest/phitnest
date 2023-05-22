f6d6cb854c63efb94ae429b40e98c1649992dc21
Jp <jpsiegel8@gmail.com>
2023-05-21 21:10:18 -0400

    Improved bloc getter

--- lib/bloc.dart
+++ lib/bloc.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-extension BlocGetter<T extends Bloc<Event, State>, State, Event>
-    on BuildContext {
-  T get bloc => BlocProvider.of(this);
+extension BlocGetter on BuildContext {
+  T bloc<T extends Bloc<Event, State>, State, Event>() => BlocProvider.of(this);
 }
