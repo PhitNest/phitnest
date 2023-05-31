a2f1b59a8d725f34b0a8158f4cc9c9baee1f6063
JP Siegel <jpsiegel8@gmail.com>
2023-05-30 20:19:23 -0400

    Updated admin presignup hook

--- src/api/cognito_hooks/admin-presignup.ts
+++ src/api/cognito_hooks/admin-presignup.ts
@@ -1,9 +1,17 @@
 import { TransactWriteItemsCommand } from "@aws-sdk/client-dynamodb";
+import { Admin, adminToDynamo } from "api/common/entities";
 import { connectDynamo } from "api/common/utils";
 import { PreSignUpTriggerEvent } from "aws-lambda";
 
 export async function invoke(event: PreSignUpTriggerEvent) {
   const client = connectDynamo();
+  const admin: Admin = {
+    accountDetails: {
+      email: event.request.userAttributes.email,
+      id: event.userName,
+      createdAt: new Date(),
+    },
+  };
   await client.send(
     new TransactWriteItemsCommand({
       TransactItems: [
@@ -13,7 +21,7 @@ export async function invoke(event: PreSignUpTriggerEvent) {
             Item: {
               part_id: { S: "ADMINS" },
               sort_id: { S: `ADMIN#${event.userName}` },
-              email: { S: event.request.userAttributes.email },
+              ...adminToDynamo(admin),
             },
           },
         },
