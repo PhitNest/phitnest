3b9b1fd9cba5eb39700c27f4baad58bdb636505a
JP Siegel <jpsiegel8@gmail.com>
2023-06-17 18:24:21 -0400

    Renamed error banner

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,5 @@
 export 'gym_entry/ui.dart';
 export 'invite_form/ui.dart';
-export 'styled_error_banner.dart';
+export 'styled_banner.dart';
 export 'styled_password_field.dart';
 export 'styled_underline_text_field.dart';
