b17e715bd8143d27cc73806938c3e37b530fa33e
JP Siegel <jpsiegel8@gmail.com>
2023-06-18 12:32:00 -0400

    Fixed logout

--- lib/screens/home/ui.dart
+++ lib/screens/home/ui.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:phitnest_core/core.dart';
 
 import '../../common/util.dart';
@@ -30,7 +31,13 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
             BlocConsumer<CognitoBloc, CognitoState>(
-              listener: (context, cognitoState) {},
+              listener: (context, cognitoState) {
+                switch (cognitoState) {
+                  case CognitoLoadedPoolInitialState():
+                    context.goNamed('login');
+                  default:
+                }
+              },
               builder: (context, cognitoState) => switch (cognitoState) {
                 CognitoLoggingOutState() => const CircularProgressIndicator(),
                 _ => TextButton(
