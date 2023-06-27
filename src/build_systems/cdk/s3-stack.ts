4228f9536833f21df602e5da04e7cc0fc665a432
JP Siegel <jpsiegel8@gmail.com>
2023-06-27 00:23:34 -0400

    Added support for .png

--- src/build_systems/cdk/s3-stack.ts
+++ src/build_systems/cdk/s3-stack.ts
@@ -64,6 +64,7 @@ export class S3Stack {
         actions: ["s3:PutObject", "s3:DeleteObject"],
         resources: [
           `${this.userBucket.bucketArn}/profilePictures/\${cognito-identity.amazonaws.com:sub}.jpg`,
+          `${this.userBucket.bucketArn}/profilePictures/\${cognito-identity.amazonaws.com:sub}.png`,
         ],
       })
     );
