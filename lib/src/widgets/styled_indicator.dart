dc846d1bfb976a800cf111eab67007cc2aca1b76
XxThunderBlastxX <koustavmondal55@gmail.com>
2023-06-28 20:46:43 +0530

    Added styled_nav_bar

---
+++ lib/src/widgets/styled_indicator.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../theme.dart';
+
+class StyledIndicator extends StatelessWidget {
+  final int count;
+  final Widget child;
+  final Size offset;
+
+  const StyledIndicator({
+    Key? key,
+    required this.child,
+    required this.count,
+    required this.offset,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        child,
+        Visibility(
+          visible: count > 0,
+          child: Positioned(
+            right: offset.width,
+            top: offset.height,
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 9.w,
+                vertical: 2.5.h,
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(32.r),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                count.toString(),
+                style: AppTheme.instance.theme.textTheme.bodySmall!.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
