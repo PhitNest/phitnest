a341cd8bc50047e41c01343473bb0abc09674cfd
JP Siegel <jpsiegel8@gmail.com>
2023-07-15 15:36:43 -0400

    Updated workflow

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { App } from "aws-cdk-lib";
 import { PhitnestStack } from "./phitnest-stack";
-import { gitClone } from "./utils/git-clone";
-
-gitClone("https://github.com/PhitNest/phitnest-api.git", "phitnest-api");
 
 const app = new App();
 new PhitnestStack(app);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from "./file-based-routing";
-export * from "./git-clone";
 export * from "./lambda-deployment";
