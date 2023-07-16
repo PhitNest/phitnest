d097ce860dc2ddfb5375601dcad7be2b126a0535
JP Siegel <jpsiegel8@gmail.com>
2023-07-16 17:49:13 -0400

    Created login provider abstraction

---
+++ lib/ui/login_provider.dart
@@ -0,0 +1,127 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../api/api.dart';
+import '../bloc/loader.dart';
+import '../cognito/cognito.dart';
+
+final class LoginFormRejectedEvent extends Equatable {
+  const LoginFormRejectedEvent() : super();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class LoginState extends Equatable {
+  final AutovalidateMode autovalidateMode;
+
+  const LoginState({
+    required this.autovalidateMode,
+  }) : super();
+
+  @override
+  List<Object?> get props => [autovalidateMode];
+}
+
+extension GetLoginBloc on BuildContext {
+  LoginBloc get loginBloc => BlocProvider.of(this);
+}
+
+final class LoginBloc extends Bloc<LoginFormRejectedEvent, LoginState> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  LoginBloc()
+      : super(
+          const LoginState(
+            autovalidateMode: AutovalidateMode.disabled,
+          ),
+        ) {
+    on<LoginFormRejectedEvent>(
+      (event, emit) => emit(
+        const LoginState(
+          autovalidateMode: AutovalidateMode.always,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    emailController.dispose();
+    passwordController.dispose();
+    return super.close();
+  }
+}
+
+final class LoginParams extends Equatable {
+  final String email;
+  final String password;
+
+  const LoginParams({
+    required this.email,
+    required this.password,
+  }) : super();
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+final class LoginProvider extends StatelessWidget {
+  final ApiInfo apiInfo;
+
+  final Widget Function(
+    BuildContext,
+    LoginState,
+    GlobalKey<FormState> formKey,
+    TextEditingController emailController,
+    TextEditingController passwordController,
+    Widget Function(
+      Widget Function(BuildContext, LoaderState<LoginResponse>) consumerBuilder,
+      void Function(BuildContext, LoaderState<LoginResponse>) consumerListener,
+    ) consumer,
+  ) formBuilder;
+
+  const LoginProvider({
+    required this.apiInfo,
+    required this.formBuilder,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) => MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => LoaderBloc<LoginParams, LoginResponse>(
+              load: (params) => login(
+                apiInfo: apiInfo,
+                password: params.password,
+                email: params.email,
+              ),
+            ),
+          ),
+          BlocProvider(
+            create: (_) => LoginBloc(),
+          ),
+        ],
+        child: BlocConsumer<LoginBloc, LoginState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            final loginBloc = context.loginBloc;
+            return formBuilder(
+              context,
+              state,
+              loginBloc.formKey,
+              loginBloc.emailController,
+              loginBloc.passwordController,
+              (consumerBuilder, consumerListener) =>
+                  LoaderConsumer<LoginParams, LoginResponse>(
+                builder: consumerBuilder,
+                listener: consumerListener,
+              ),
+            );
+          },
+        ),
+      );
+}
---
+++ lib/ui/restore_session_provider.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../api/api.dart';
+import '../bloc/loader.dart';
+import '../cognito/cognito.dart';
+import '../http/http.dart';
+import 'styled/styled_banner.dart';
+
+final class RestoreSessionProvider extends StatelessWidget {
+  final Widget loader;
+  final bool useAdminAuth;
+  final void Function(BuildContext, Session) onSessionRestored;
+  final void Function(BuildContext, ApiInfo) onSessionRestoreFailed;
+
+  const RestoreSessionProvider({
+    super.key,
+    required this.loader,
+    required this.onSessionRestored,
+    required this.onSessionRestoreFailed,
+    required this.useAdminAuth,
+  }) : super();
+
+  @override
+  Widget build(BuildContext context) => BlocProvider(
+        create: (_) => LoaderBloc(
+          load: (_) => requestApiInfo(
+            readFromCache: true,
+            writeToCache: true,
+            useAdmin: useAdminAuth,
+          ),
+          loadOnStart: (req: null),
+        ),
+        child: LoaderConsumer<void, HttpResponse<ApiInfo>>(
+          listener: (context, apiInfoState) {
+            switch (apiInfoState) {
+              case LoaderLoadedState(data: final apiInfoResponse):
+                switch (apiInfoResponse) {
+                  // This indicates that we failed to load API info from the
+                  // server
+                  case HttpResponseFailure(failure: final failure):
+                    // Show error message if we failed to load API info from
+                    // server
+                    StyledBanner.show(
+                      message: failure.message,
+                      error: true,
+                    );
+                    // Retry loading API info from server
+                    context
+                        .loader<void, HttpResponse<ApiInfo>>()
+                        .add(const LoaderLoadEvent(null));
+                  // This indicates that we successfully loaded API info from
+                  // server, but our cache was empty so a session will not be
+                  // restored.
+                  case HttpResponseOk(data: final apiInfo):
+                    onSessionRestoreFailed(context, apiInfo);
+                  case HttpResponseCache():
+                }
+              default:
+            }
+          },
+          builder: (context, apiInfoState) => switch (apiInfoState) {
+            LoaderInitialState() || LoaderLoadingState() => loader,
+            LoaderLoadedState(data: final apiInfoResponse) => switch (
+                  apiInfoResponse) {
+                // This indicates that we successfully loaded API info from the
+                // cache, so a session might be restorable.
+                HttpResponseCache(data: final cachedApiInfo) => BlocProvider(
+                    create: (_) => LoaderBloc(
+                      load: (_) => getPreviousSession(cachedApiInfo),
+                      loadOnStart: (req: null),
+                    ),
+                    child: LoaderConsumer<ApiInfo, RefreshSessionResponse>(
+                      listener: (context, restoreSessionState) {
+                        switch (restoreSessionState) {
+                          case LoaderLoadedState(
+                              data: final restoreSessionResponse
+                            ):
+                            switch (restoreSessionResponse) {
+                              case RefreshSessionFailureResponse():
+                                onSessionRestoreFailed(
+                                  context,
+                                  cachedApiInfo,
+                                );
+                              case RefreshSessionSuccess(
+                                  newSession: final newSession
+                                ):
+                                onSessionRestored(context, newSession);
+                            }
+                          default:
+                        }
+                      },
+                      builder: (context, restoreSessionState) => loader,
+                    ),
+                  ),
+                _ => loader,
+              }
+          },
+        ),
+      );
+}
