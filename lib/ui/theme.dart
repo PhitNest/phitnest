5ce2c8cd378a044dec550af57b4a99fa8728cb2b
JP Siegel <jpsiegel8@gmail.com>
2023-07-18 10:07:20 -0400

    Fixed screen utils

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -3,87 +3,94 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  AppTheme._();
+  static var instance = AppTheme._(false);
+  static set useScreenUtils(bool useScreenUtils) =>
+      instance = AppTheme._(useScreenUtils);
 
-  static final instance = AppTheme._();
-
-  final ThemeData theme = ThemeData(
-    scaffoldBackgroundColor: const Color(0xFF000000),
-    colorScheme: const ColorScheme(
-      brightness: Brightness.dark,
-      primary: Color(0xFF5E5CE6),
-      onPrimary: Color(0xFF5E5CE6),
-      secondary: Color(0xFFF4F9FF),
-      onSecondary: Color(0xFFF4F9FF),
-      error: Color(0xFFF4F9FF),
-      onError: Color(0xFFF4F9FF),
-      background: Color(0xFF000000),
-      onBackground: Color(0xFF000000),
-      surface: Color(0xFF000000),
-      onSurface: Color(0xFF000000),
-    ),
-    textTheme: TextTheme(
-      bodyLarge: GoogleFonts.inter(
-        fontSize: 28.sp,
-        fontWeight: FontWeight.w700,
-        height: 1.25,
-        color: const Color(0xFFF8FEFF),
+  final bool screenUtils;
+  late final ThemeData theme;
+  AppTheme._(this.screenUtils) {
+    theme = ThemeData(
+      scaffoldBackgroundColor: const Color(0xFF000000),
+      colorScheme: const ColorScheme(
+        brightness: Brightness.dark,
+        primary: Color(0xFF5E5CE6),
+        onPrimary: Color(0xFF5E5CE6),
+        secondary: Color(0xFFF4F9FF),
+        onSecondary: Color(0xFFF4F9FF),
+        error: Color(0xFFF4F9FF),
+        onError: Color(0xFFF4F9FF),
+        background: Color(0xFF000000),
+        onBackground: Color(0xFF000000),
+        surface: Color(0xFF000000),
+        onSurface: Color(0xFF000000),
       ),
-      bodyMedium: GoogleFonts.inter(
-        fontSize: 20.sp,
-        fontWeight: FontWeight.w500,
-        height: 1.25,
-        color: const Color(0xFFF8FEFF),
-      ),
-      bodySmall: GoogleFonts.inter(
-        fontSize: 12.sp,
-        fontWeight: FontWeight.w700,
-        height: 1.25,
-        color: const Color(0xFFF8FEFF),
-      ),
-    ),
-    appBarTheme: AppBarTheme(
-      iconTheme: const IconThemeData(
-        color: Color(0xFFF8FEFF),
-      ),
-      titleTextStyle: GoogleFonts.inter(
-        fontSize: 20.sp,
-        fontWeight: FontWeight.w500,
-        height: 1.25,
-        color: const Color(0xFFF8FEFF),
-      ),
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: ButtonStyle(
-        padding: MaterialStateProperty.all(
-          EdgeInsets.symmetric(horizontal: 24.w, vertical: 18.h),
+      textTheme: TextTheme(
+        bodyLarge: GoogleFonts.inter(
+          fontSize: screenUtils ? 28.sp : 28,
+          fontWeight: FontWeight.w700,
+          height: 1.25,
+          color: const Color(0xFFF8FEFF),
         ),
-        backgroundColor: MaterialStateProperty.all(
-          const Color(0xFF5E5CE6),
+        bodyMedium: GoogleFonts.inter(
+          fontSize: screenUtils ? 20.sp : 20,
+          fontWeight: FontWeight.w500,
+          height: 1.25,
+          color: const Color(0xFFF8FEFF),
         ),
-        shape: MaterialStateProperty.all(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.r),
+        bodySmall: GoogleFonts.inter(
+          fontSize: screenUtils ? 12.sp : 12,
+          fontWeight: FontWeight.w700,
+          height: 1.25,
+          color: const Color(0xFFF8FEFF),
+        ),
+      ),
+      appBarTheme: AppBarTheme(
+        iconTheme: const IconThemeData(
+          color: Color(0xFFF8FEFF),
+        ),
+        titleTextStyle: GoogleFonts.inter(
+          fontSize: screenUtils ? 20.sp : 20,
+          fontWeight: FontWeight.w500,
+          height: 1.25,
+          color: const Color(0xFFF8FEFF),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all(
+            EdgeInsets.symmetric(
+              horizontal: screenUtils ? 24.w : 24,
+              vertical: screenUtils ? 18.h : 18,
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all(
+            const Color(0xFF5E5CE6),
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(screenUtils ? 50.r : 50),
+            ),
           ),
         ),
       ),
-    ),
-    textButtonTheme: TextButtonThemeData(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.transparent),
-        textStyle: MaterialStateProperty.all(
-          GoogleFonts.inter(
-            fontSize: 14.sp,
-            fontWeight: FontWeight.w400,
-            fontStyle: FontStyle.italic,
-            height: 1.25,
-            color: const Color(0xFFF4F9FF),
-            decoration: TextDecoration.underline,
-            decorationColor: Colors.white,
-            decorationThickness: 1.5,
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.transparent),
+          textStyle: MaterialStateProperty.all(
+            GoogleFonts.inter(
+              fontSize: screenUtils ? 14.sp : 14,
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.italic,
+              height: 1.25,
+              color: const Color(0xFFF4F9FF),
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.white,
+              decorationThickness: 1.5,
+            ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
