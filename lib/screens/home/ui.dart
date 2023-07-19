b0e23e045e8939c6527b82c1e9b79fbfbc61b6dd
JohnSiegel <jpsiegel8@gmail.com>
2023-07-19 16:58:30 -0400

    Update for demo

--- lib/screens/home/ui.dart
+++ lib/screens/home/ui.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:phitnest_core/core.dart';
 
 import '../../widgets/widgets.dart';
+import '../login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   final ApiInfo apiInfo;
@@ -13,14 +16,99 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        body: StyledNavBar(
-          listener: (context, navBarState) async {},
-          builder: (context, navBarState) => switch (navBarState.page) {
-            NavBarPage.news => Container(),
-            NavBarPage.explore => Container(),
-            NavBarPage.chat => Container(),
-            NavBarPage.options => Container(),
-          },
+        body: BlocProvider(
+          create: (context) => LoaderBloc<void, void>(
+            load: (_) async {
+              final session = await context.sessionLoader.session;
+              if (session != null) {
+                await logout(session: session);
+              }
+            },
+          ),
+          child: StyledNavBar(
+            listener: (context, navBarState) async {},
+            builder: (context, navBarState) => LoaderConsumer<void, void>(
+              listener: (context, loaderState) {
+                switch (loaderState) {
+                  case LoaderLoadedState():
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      CupertinoPageRoute<void>(
+                        builder: (context) => LoginScreen(apiInfo: apiInfo),
+                      ),
+                      (_) => false,
+                    );
+                  default:
+                }
+              },
+              builder: (context, loaderState) => switch (navBarState.page) {
+                NavBarPage.news => Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('News screen'),
+                      switch (loaderState) {
+                        LoaderLoadingState() =>
+                          const CircularProgressIndicator(),
+                        _ => StyledOutlineButton(
+                            onPress: () => context
+                                .loader<void, void>()
+                                .add(const LoaderLoadEvent(null)),
+                            text: 'Logout',
+                          ),
+                      },
+                    ],
+                  ),
+                NavBarPage.explore => Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Explore screen'),
+                      switch (loaderState) {
+                        LoaderLoadingState() =>
+                          const CircularProgressIndicator(),
+                        _ => StyledOutlineButton(
+                            onPress: () => context
+                                .loader<void, void>()
+                                .add(const LoaderLoadEvent(null)),
+                            text: 'Logout',
+                          ),
+                      },
+                    ],
+                  ),
+                NavBarPage.chat => Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Chat screen'),
+                      switch (loaderState) {
+                        LoaderLoadingState() =>
+                          const CircularProgressIndicator(),
+                        _ => StyledOutlineButton(
+                            onPress: () => context
+                                .loader<void, void>()
+                                .add(const LoaderLoadEvent(null)),
+                            text: 'Logout',
+                          ),
+                      },
+                    ],
+                  ),
+                NavBarPage.options => Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Options screen'),
+                      switch (loaderState) {
+                        LoaderLoadingState() =>
+                          const CircularProgressIndicator(),
+                        _ => StyledOutlineButton(
+                            onPress: () => context
+                                .loader<void, void>()
+                                .add(const LoaderLoadEvent(null)),
+                            text: 'Logout',
+                          ),
+                      },
+                    ],
+                  ),
+              },
+            ),
+          ),
         ),
       );
 }
