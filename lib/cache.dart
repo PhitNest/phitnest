e81f0fa75b34977e256e190f41aad73b900203a9
JP Siegel <jpsiegel8@gmail.com>
2023-07-29 13:45:45 -0400

    Refactored s3

--- lib/cache.dart
+++ lib/cache.dart
@@ -5,22 +5,24 @@ import 'package:synchronized/synchronized.dart';
 import 'core.dart';
 
 final class _CacheInternals {
-// This will be set to true when the cache is initialized
-  final SharedPreferences _sharedPreferences;
-  final FlutterSecureStorage _secureStorage;
-  final _secureCacheLock = Lock();
+  final bool firstRun;
+
+  final SharedPreferences sharedPreferences;
+  final FlutterSecureStorage secureStorage;
+  final secureCacheLock = Lock();
 
   // secure cache variables
-  final Map<String, String> _stringifiedSecureCache;
-  final Map<String, Serializable> _lazyLoadedSecureCache;
-  final Map<String, List<Serializable>> _lazyLoadedSecureListCache;
+  final Map<String, String> stringifiedSecureCache;
+  final Map<String, Serializable> lazyLoadedSecureCache;
+  final Map<String, List<Serializable>> lazyLoadedSecureListCache;
 
   _CacheInternals(
-    this._sharedPreferences,
-    this._secureStorage,
-    this._stringifiedSecureCache,
-    this._lazyLoadedSecureCache,
-    this._lazyLoadedSecureListCache,
+    this.firstRun,
+    this.sharedPreferences,
+    this.secureStorage,
+    this.stringifiedSecureCache,
+    this.lazyLoadedSecureCache,
+    this.lazyLoadedSecureListCache,
   );
 
   /// Function to initialize cache
@@ -33,14 +35,18 @@ final class _CacheInternals {
       aOptions: AndroidOptions(encryptedSharedPreferences: true),
       iOptions: IOSOptions(accessibility: KeychainAccessibility.first_unlock),
     );
+    final firstRun = sharedPreferences.getBool('first_run') ?? true;
+
     // Clear secure storage on first run
-    if (sharedPreferences.getBool('first_run') ?? true) {
+    if (firstRun) {
       await secureStorage.deleteAll();
       await sharedPreferences.setBool('first_run', false);
     }
+
     // Read all secure storage values into cache
     stringifiedSecureCache.addAll(await secureStorage.readAll());
     return _CacheInternals(
+      firstRun,
       sharedPreferences,
       secureStorage,
       stringifiedSecureCache,
@@ -66,20 +72,20 @@ Future<void> cacheSecureObject(String key, Serializable? value) async {
   if (value != null) {
     prettyLogger
         .d("Caching Object:\n\tkey: $key${wrapText("\n\tvalue: $value")}");
-    await _cache._secureCacheLock.synchronized(() async {
-      _cache._stringifiedSecureCache[key] = jsonEncode(value.toJson());
-      _cache._lazyLoadedSecureCache[key] = value;
-      await _cache._secureStorage
-          .write(key: key, value: _cache._stringifiedSecureCache[key]);
+    await _cache.secureCacheLock.synchronized(() async {
+      _cache.stringifiedSecureCache[key] = jsonEncode(value.toJson());
+      _cache.lazyLoadedSecureCache[key] = value;
+      await _cache.secureStorage
+          .write(key: key, value: _cache.stringifiedSecureCache[key]);
     });
   } else {
-    await _cache._secureCacheLock.synchronized(() async {
+    await _cache.secureCacheLock.synchronized(() async {
       prettyLogger.d('Removing Cached Object:\n\tkey: $key'
           '${wrapText("\n\tvalue: ${(() {
-        return _cache._lazyLoadedSecureCache.remove(key);
+        return _cache.lazyLoadedSecureCache.remove(key);
       })()}")}');
-      _cache._stringifiedSecureCache.remove(key);
-      await _cache._secureStorage.delete(key: key);
+      _cache.stringifiedSecureCache.remove(key);
+      await _cache.secureStorage.delete(key: key);
     });
   }
 }
@@ -89,11 +95,11 @@ T? getSecureCachedObject<T extends Serializable>(
   String key,
   T Function(Map<String, dynamic>) parseJson,
 ) {
-  if (_cache._lazyLoadedSecureCache[key] != null) {
-    return _cache._lazyLoadedSecureCache[key] as T;
-  } else if (_cache._stringifiedSecureCache[key] != null) {
-    return _cache._lazyLoadedSecureCache[key] = parseJson(
-        jsonDecode(_cache._stringifiedSecureCache[key]!)
+  if (_cache.lazyLoadedSecureCache[key] != null) {
+    return _cache.lazyLoadedSecureCache[key] as T;
+  } else if (_cache.stringifiedSecureCache[key] != null) {
+    return _cache.lazyLoadedSecureCache[key] = parseJson(
+        jsonDecode(_cache.stringifiedSecureCache[key]!)
             as Map<String, dynamic>);
   } else {
     return null;
@@ -105,11 +111,11 @@ List<T>? getSecureCachedList<T extends Serializable>(
   String key,
   T Function(Map<String, dynamic>) parseJson,
 ) {
-  if (_cache._lazyLoadedSecureListCache[key] != null) {
-    return _cache._lazyLoadedSecureListCache[key] as List<T>;
-  } else if (_cache._stringifiedSecureCache[key] != null) {
-    return _cache._lazyLoadedSecureListCache[key] =
-        (jsonDecode(_cache._stringifiedSecureCache[key]!) as List)
+  if (_cache.lazyLoadedSecureListCache[key] != null) {
+    return _cache.lazyLoadedSecureListCache[key] as List<T>;
+  } else if (_cache.stringifiedSecureCache[key] != null) {
+    return _cache.lazyLoadedSecureListCache[key] =
+        (jsonDecode(_cache.stringifiedSecureCache[key]!) as List)
             .map((e) => parseJson(e as Map<String, dynamic>))
             .toList()
             .cast<T>();
@@ -126,59 +132,58 @@ Future<void> cacheSecureList(
   if (value != null) {
     prettyLogger
         .d("Caching List:\n\tkey: $key${wrapText("\n\tvalue: $value")}");
-    await _cache._secureCacheLock.synchronized(() async {
-      _cache._stringifiedSecureCache[key] =
+    await _cache.secureCacheLock.synchronized(() async {
+      _cache.stringifiedSecureCache[key] =
           jsonEncode(value.map((e) => e.toJson()).toList());
-      _cache._lazyLoadedSecureListCache[key] = value;
-      await _cache._secureStorage
-          .write(key: key, value: _cache._stringifiedSecureCache[key]);
+      _cache.lazyLoadedSecureListCache[key] = value;
+      await _cache.secureStorage
+          .write(key: key, value: _cache.stringifiedSecureCache[key]);
     });
   } else {
-    await _cache._secureCacheLock.synchronized(() async {
-      _cache._stringifiedSecureCache.remove(key);
+    await _cache.secureCacheLock.synchronized(() async {
+      _cache.stringifiedSecureCache.remove(key);
       prettyLogger.d(
         'Removing Cached List:\n\tkey: $key'
         '${wrapText("\n\tvalue: ${() {
-          _cache._lazyLoadedSecureListCache.remove(key);
+          _cache.lazyLoadedSecureListCache.remove(key);
         }()}")}',
       );
-      await _cache._secureStorage.delete(key: key);
+      await _cache.secureStorage.delete(key: key);
     });
   }
 }
 
 /// Function to cache secure strings
 Future<void> cacheSecureString(String key, String? value) async {
-  await _cache._secureCacheLock.synchronized(() async {
+  await _cache.secureCacheLock.synchronized(() async {
     if (value != null) {
       prettyLogger
           .d("Caching String:\n\tkey: $key${wrapText("\n\tvalue: $value")}");
-      _cache._stringifiedSecureCache[key] = value;
+      _cache.stringifiedSecureCache[key] = value;
     } else {
       prettyLogger.d('Removing Cached String:\n\tkey: $key'
           '${wrapText("old value: ${() {
-        _cache._stringifiedSecureCache.remove(key);
+        _cache.stringifiedSecureCache.remove(key);
       }()}")}');
     }
-    await _cache._secureStorage.write(key: key, value: value);
+    await _cache.secureStorage.write(key: key, value: value);
   });
 }
 
 /// Function to get cached secure strings
-String? getCachedSecureString(String key) =>
-    _cache._stringifiedSecureCache[key];
+String? getCachedSecureString(String key) => _cache.stringifiedSecureCache[key];
 
 /// Shorthand string getter for SharedPreferences (insecure)
-final getCachedString = _cache._sharedPreferences.getString;
+final getCachedString = _cache.sharedPreferences.getString;
 
 /// Shorthand int getter for SharedPreferences (insecure)
-final getCachedInt = _cache._sharedPreferences.getInt;
+final getCachedInt = _cache.sharedPreferences.getInt;
 
 /// Shorthand bool getter for SharedPreferences (insecure)
-final getCachedBool = _cache._sharedPreferences.getBool;
+final getCachedBool = _cache.sharedPreferences.getBool;
 
 /// Shorthand double getter for SharedPreferences (insecure)
-final getCachedDouble = _cache._sharedPreferences.getDouble;
+final getCachedDouble = _cache.sharedPreferences.getDouble;
 
 /// Function to cache objects in SharedPreferences (insecure)
 Future<void> cacheObject<T extends Serializable>(
@@ -186,13 +191,13 @@ Future<void> cacheObject<T extends Serializable>(
   T? value,
 ) =>
     value != null
-        ? _cache._sharedPreferences.setString(key, jsonEncode(value.toJson()))
-        : _cache._sharedPreferences.remove(key);
+        ? _cache.sharedPreferences.setString(key, jsonEncode(value.toJson()))
+        : _cache.sharedPreferences.remove(key);
 
 /// Function to get cached objects from SharedPreferences (insecure)
 T? getCachedObject<T extends Serializable>(
     String key, T Function(Map<String, dynamic> json) parser) {
-  final result = _cache._sharedPreferences.getString(key);
+  final result = _cache.sharedPreferences.getString(key);
   return result != null
       ? parser(jsonDecode(result) as Map<String, dynamic>)
       : null;
@@ -204,32 +209,32 @@ Future<void> _cachePrimitive<T>(
   T? value,
   Future<void> Function(String, T) setter,
 ) =>
-    value != null ? setter(key, value) : _cache._sharedPreferences.remove(key);
+    value != null ? setter(key, value) : _cache.sharedPreferences.remove(key);
 
 /// Function to cache strings in SharedPreferences (insecure)
 Future<void> cacheString(
   String key,
   String? value,
 ) =>
-    _cachePrimitive(key, value, _cache._sharedPreferences.setString);
+    _cachePrimitive(key, value, _cache.sharedPreferences.setString);
 
 /// Function to cache ints in SharedPreferences (insecure)
 Future<void> cacheInt(
   String key,
   int? value,
 ) =>
-    _cachePrimitive(key, value, _cache._sharedPreferences.setInt);
+    _cachePrimitive(key, value, _cache.sharedPreferences.setInt);
 
 /// Function to cache bools in SharedPreferences (insecure)
 Future<void> cacheBool(
   String key,
   bool? value,
 ) =>
-    _cachePrimitive(key, value, _cache._sharedPreferences.setBool);
+    _cachePrimitive(key, value, _cache.sharedPreferences.setBool);
 
 /// Function to cache doubles in SharedPreferences (insecure)
 Future<void> cacheDouble(
   String key,
   double? value,
 ) =>
-    _cachePrimitive(key, value, _cache._sharedPreferences.setDouble);
+    _cachePrimitive(key, value, _cache.sharedPreferences.setDouble);
--- lib/cognito/s3.dart
+++ lib/cognito/s3.dart
@@ -2,18 +2,17 @@ part of 'cognito.dart';
 
 const kS3Service = 's3';
 
-({Uri uri, Map<String, String> headers})? getProfilePictureUri(
+({Uri uri, Map<String, String> headers}) getProfilePictureUri(
   Session session,
   String identityId,
 ) {
-  try {
-    final key =
-        '${session.apiInfo.userBucketName}/profilePictures/$identityId.txt';
-    final payload = SigV4.hashCanonicalRequest('');
-    final datetime = SigV4.generateDatetime();
-    final region = session.user.pool.getRegion()!;
-    final host = '$kS3Service.$region.amazonaws.com';
-    final canonicalRequest = '''GET
+  final key =
+      '${session.apiInfo.userBucketName}/profilePictures/$identityId.txt';
+  final payload = SigV4.hashCanonicalRequest('');
+  final datetime = SigV4.generateDatetime();
+  final region = session.user.pool.getRegion()!;
+  final host = '$kS3Service.$region.amazonaws.com';
+  final canonicalRequest = '''GET
 ${'/$key'.split('/').map((s) => Uri.encodeComponent(s)).join('/')}
 
 host:$host
@@ -23,123 +22,31 @@ x-amz-security-token:${session.credentials.sessionToken}
 
 host;x-amz-content-sha256;x-amz-date;x-amz-security-token
 $payload''';
-    final credentialScope =
-        SigV4.buildCredentialScope(datetime, region, kS3Service);
-    final stringToSign = SigV4.buildStringToSign(datetime, credentialScope,
-        SigV4.hashCanonicalRequest(canonicalRequest));
-    final signingKey = SigV4.calculateSigningKey(
-        session.credentials.secretAccessKey!, datetime, region, kS3Service);
-    final signature = SigV4.calculateSignature(signingKey, stringToSign);
+  final credentialScope =
+      SigV4.buildCredentialScope(datetime, region, kS3Service);
+  final stringToSign = SigV4.buildStringToSign(
+      datetime, credentialScope, SigV4.hashCanonicalRequest(canonicalRequest));
+  final signingKey = SigV4.calculateSigningKey(
+      session.credentials.secretAccessKey!, datetime, region, kS3Service);
+  final signature = SigV4.calculateSignature(signingKey, stringToSign);
 
-    final authorization = [
-      'AWS4-HMAC-SHA256 Credential=${session.credentials.accessKeyId}/$credentialScope',
-      'SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token',
-      'Signature=$signature',
-    ].join(',');
+  final authorization = [
+    'AWS4-HMAC-SHA256 Credential=${session.credentials.accessKeyId}/$credentialScope',
+    'SignedHeaders=host;x-amz-content-sha256;x-amz-date;x-amz-security-token',
+    'Signature=$signature',
+  ].join(',');
 
-    final uri = Uri.https(host, key);
+  final uri = Uri.https(host, key);
 
-    return (
-      uri: uri,
-      headers: {
-        'Authorization': authorization,
-        'x-amz-content-sha256': payload,
-        'x-amz-date': datetime,
-        'x-amz-security-token': session.credentials.sessionToken!,
-      }
-    );
-  } catch (e) {
-    return null;
-  }
-}
-
-Future<Image?> getProfilePicture(Session session, String identityId) async {
-  try {
-    final params = getProfilePictureUri(session, identityId);
-    if (params == null) {
-      return null;
-    } else {
-      final res = await http.get(params.uri, headers: params.headers);
-      if (res.statusCode == 200) {
-        return Image.memory(res.bodyBytes);
-      } else {
-        return null;
-      }
+  return (
+    uri: uri,
+    headers: {
+      'Authorization': authorization,
+      'x-amz-content-sha256': payload,
+      'x-amz-date': datetime,
+      'x-amz-security-token': session.credentials.sessionToken!,
     }
-  } catch (e) {
-    return null;
-  }
-}
-
-final class Policy {
-  String expiration;
-  String bucket;
-  String key;
-  String credential;
-  String datetime;
-  String sessionToken;
-  int maxFileSize;
-
-  Policy(
-    this.key,
-    this.bucket,
-    this.datetime,
-    this.expiration,
-    this.credential,
-    this.maxFileSize,
-    this.sessionToken,
   );
-
-  factory Policy.fromS3PresignedPost(
-    String key,
-    String bucket,
-    int expiryMinutes,
-    String accessKeyId,
-    int maxFileSize,
-    String sessionToken,
-  ) {
-    final datetime = SigV4.generateDatetime();
-    final expiration = (DateTime.now())
-        .add(Duration(minutes: expiryMinutes))
-        .toUtc()
-        .toString()
-        .split(' ')
-        .join('T');
-    final cred =
-        '$accessKeyId/${SigV4.buildCredentialScope(datetime, 'us-east-1', 's3')}';
-    final p = Policy(
-      key,
-      bucket,
-      datetime,
-      expiration,
-      cred,
-      maxFileSize,
-      sessionToken,
-    );
-    return p;
-  }
-
-  String encode() {
-    final bytes = utf8.encode(toString());
-    return base64.encode(bytes);
-  }
-
-  @override
-  String toString() {
-    return '''
-    { "expiration": "$expiration",
-      "conditions": [
-        {"bucket": "$bucket"},
-        ["starts-with", "\$key", "$key"],
-        ["content-length-range", 1, $maxFileSize],
-        {"x-amz-credential": "$credential"},
-        {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
-        {"x-amz-date": "$datetime" },
-        {"x-amz-security-token": "$sessionToken" }
-      ]
-    }
-    ''';
-  }
 }
 
 Future<Failure?> uploadProfilePicture({
@@ -163,29 +70,45 @@ Future<Failure?> uploadProfilePicture({
       photo,
       length,
     );
-
-    final policy = Policy.fromS3PresignedPost(
-      bucketKey,
-      session.apiInfo.userBucketName,
-      15,
-      session.credentials.accessKeyId!,
-      length,
-      session.credentials.sessionToken!,
-    );
+    final accessKeyId = session.credentials.accessKeyId!;
+    final datetime = SigV4.generateDatetime();
+    final expiration = (DateTime.now())
+        .add(Duration(minutes: 15))
+        .toUtc()
+        .toString()
+        .split(' ')
+        .join('T');
+    final cred =
+        '$accessKeyId/${SigV4.buildCredentialScope(datetime, 'us-east-1', 's3')}';
     final key = SigV4.calculateSigningKey(
       session.credentials.secretAccessKey!,
-      policy.datetime,
+      datetime,
       session.user.pool.getRegion()!,
       's3',
     );
-    final signature = SigV4.calculateSignature(key, policy.encode());
+
+    final policy = base64.encode(utf8.encode('''
+{ "expiration": "$expiration",
+  "conditions": [
+    {"bucket": "${session.apiInfo.userBucketName}"},
+    ["starts-with", "\$key", "$bucketKey"],
+    ["content-length-range", 1, $length],
+    {"x-amz-credential": "$cred"},
+    {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
+    {"x-amz-date": "$datetime" },
+    {"x-amz-security-token": "${session.credentials.sessionToken!}" }
+  ]
+}
+'''));
+
+    final signature = SigV4.calculateSignature(key, policy);
 
     req.files.add(multipartFile);
-    req.fields['key'] = policy.key;
-    req.fields['X-Amz-Credential'] = policy.credential;
+    req.fields['key'] = bucketKey;
+    req.fields['X-Amz-Credential'] = cred;
     req.fields['X-Amz-Algorithm'] = 'AWS4-HMAC-SHA256';
-    req.fields['X-Amz-Date'] = policy.datetime;
-    req.fields['Policy'] = policy.encode();
+    req.fields['X-Amz-Date'] = datetime;
+    req.fields['Policy'] = policy;
     req.fields['X-Amz-Signature'] = signature;
     req.fields['x-amz-security-token'] = session.credentials.sessionToken!;
 
