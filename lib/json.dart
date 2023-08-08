8010c133b7c62402fc7ed13b369c36301f8a12ca
John Siegel <jpsiegel8@gmail.com>
2023-08-07 22:47:15 -0400

    Perfected the JSON library

--- lib/json.dart
+++ lib/json.dart
@@ -1,29 +1,47 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:equatable/equatable.dart';
+
+final class JsonMember<T, Serialized> {
+  final T value;
+  final Serialized serialized;
+
+  const JsonMember(this.value, this.serialized) : super();
+}
 
 sealed class JsonKey<T, Serialized> extends Equatable {
   final String key;
-  late final T _value;
-  late final Serialized _serialized;
+  JsonMember<T, Serialized>? member;
 
   JsonKey(this.key) : super();
 
+  JsonKey.populate(this.key, T val) : super() {
+    value = val;
+  }
+
   set serialized(Serialized data) {
-    _serialized = data;
-    _value = parse();
+    member = JsonMember(parse(data), data);
   }
 
   set value(T val) {
-    _value = val;
-    _serialized = serialize();
+    member = JsonMember(val, serialize(val));
   }
 
-  T get value => _value;
+  T get value => _get(member?.value);
 
-  Serialized get serialized => _serialized;
+  Serialized get serialized => _get(member?.serialized);
 
-  Serialized serialize();
+  V _get<V>(V? val) {
+    if (val != null) {
+      return val;
+    } else {
+      throw StateError('$key has not been parsed yet');
+    }
+  }
 
-  T parse();
+  Serialized serialize(T data);
+
+  T parse(Serialized data);
 
   @override
   List<Object?> get props => [value];
@@ -32,76 +50,99 @@ sealed class JsonKey<T, Serialized> extends Equatable {
 abstract class JsonPrimitive<T> extends JsonKey<T, T> {
   JsonPrimitive(super.key) : super();
 
-  @override
-  T serialize() => value;
+  JsonPrimitive.populated(super.key, super.val) : super.populate();
 
   @override
-  T parse() => serialized;
+  T serialize(T data) => data;
+
+  @override
+  T parse(T data) => data;
 }
 
 final class JsonString extends JsonPrimitive<String> {
   JsonString(super.key) : super();
+
+  JsonString.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonInt extends JsonPrimitive<int> {
   JsonInt(super.key) : super();
+
+  JsonInt.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonDouble extends JsonPrimitive<double> {
   JsonDouble(super.key) : super();
+
+  JsonDouble.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonBool extends JsonPrimitive<bool> {
   JsonBool(super.key) : super();
+
+  JsonBool.populated(super.key, super.val) : super.populated();
 }
 
 class JsonObject<T extends Json> extends JsonKey<T, Map<String, dynamic>> {
-  final T Function() parserConstructor;
+  late final T Function() parserConstructor;
 
   JsonObject(super.key, this.parserConstructor) : super();
 
-  @override
-  Map<String, dynamic> serialize() => value.toJson();
+  JsonObject.populated(super.key, super.val) : super.populate() {
+    parserConstructor = () {
+      throw StateError('Cannot parse a populated object');
+    };
+  }
 
   @override
-  T parse() => parserConstructor().._parse(serialized);
+  Map<String, dynamic> serialize(T data) => data.toJson();
+
+  @override
+  T parse(Map<String, dynamic> data) => parserConstructor().._parse(data);
 }
 
-abstract class JsonCollection<T, Serializable, Coll, SerializationColl>
-    extends JsonKey<Coll, SerializationColl> {
-  JsonCollection(super.key) : super();
+abstract class JsonList<T, Serializable>
+    extends JsonKey<List<T>, List<Serializable>> {
+  JsonList(super.key) : super();
+
+  JsonList.populated(super.key, super.val) : super.populate();
+
+  @override
+  List<Serializable> serialize(List<T> data) =>
+      data.map((e) => serializeItem(e)).toList();
+
+  @override
+  List<T> parse(List<Serializable> data) =>
+      data.map((e) => parseItem(e)).toList();
 
   Serializable serializeItem(T item);
 
   T parseItem(Serializable item);
 }
 
-abstract class JsonList<T, Serializable>
-    extends JsonCollection<T, Serializable, List<T>, List<Serializable>> {
-  JsonList(super.key) : super();
-
-  @override
-  List<Serializable> serialize() => value.map((e) => serializeItem(e)).toList();
-
-  @override
-  List<T> parse() => serialized.map((e) => parseItem(e)).toList();
-}
-
-abstract class JsonMap<T, Serializable> extends JsonCollection<T, Serializable,
-    Map<String, T>, Map<String, Serializable>> {
+abstract class JsonMap<T, Serializable>
+    extends JsonKey<Map<String, T>, Map<String, Serializable>> {
   JsonMap(super.key) : super();
 
-  @override
-  Map<String, Serializable> serialize() =>
-      value.map((key, val) => MapEntry(key, serializeItem(val)));
+  JsonMap.populated(super.key, super.val) : super.populate();
 
   @override
-  Map<String, T> parse() =>
-      serialized.map((key, val) => MapEntry(key, parseItem(val)));
+  Map<String, Serializable> serialize(Map<String, T> data) =>
+      data.map((key, val) => MapEntry(key, serializeItem(val)));
+
+  @override
+  Map<String, T> parse(Map<String, Serializable> data) =>
+      data.map((key, val) => MapEntry(key, parseItem(val)));
+
+  Serializable serializeItem(T item);
+
+  T parseItem(Serializable item);
 }
 
 abstract class JsonPrimitiveList<T> extends JsonList<T, T> {
   JsonPrimitiveList(super.key) : super();
+
+  JsonPrimitiveList.populated(super.key, super.val) : super.populated();
 
   @override
   T serializeItem(T item) => item;
@@ -112,25 +153,39 @@ abstract class JsonPrimitiveList<T> extends JsonList<T, T> {
 
 final class JsonStringList extends JsonPrimitiveList<String> {
   JsonStringList(super.key) : super();
+
+  JsonStringList.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonIntList extends JsonPrimitiveList<int> {
   JsonIntList(super.key) : super();
+
+  JsonIntList.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonDoubleList extends JsonPrimitiveList<double> {
   JsonDoubleList(super.key) : super();
+
+  JsonDoubleList.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonBoolList extends JsonPrimitiveList<bool> {
   JsonBoolList(super.key) : super();
+
+  JsonBoolList.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonObjectList<T extends Json>
     extends JsonList<T, Map<String, dynamic>> {
-  final T Function() parserConstructor;
+  late final T Function() parserConstructor;
 
   JsonObjectList(super.key, this.parserConstructor) : super();
+
+  JsonObjectList.populated(super.key, super.val) : super.populated() {
+    parserConstructor = () {
+      throw StateError('Cannot parse a populated object');
+    };
+  }
 
   @override
   T parseItem(Map<String, dynamic> item) => parserConstructor().._parse(item);
@@ -142,6 +197,8 @@ final class JsonObjectList<T extends Json>
 abstract class JsonPrimitiveMap<T> extends JsonMap<T, T> {
   JsonPrimitiveMap(super.key) : super();
 
+  JsonPrimitiveMap.populated(super.key, super.val) : super.populated();
+
   @override
   T serializeItem(T item) => item;
 
@@ -151,25 +208,39 @@ abstract class JsonPrimitiveMap<T> extends JsonMap<T, T> {
 
 final class JsonStringMap extends JsonPrimitiveMap<String> {
   JsonStringMap(super.key) : super();
+
+  JsonStringMap.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonIntMap extends JsonPrimitiveMap<int> {
   JsonIntMap(super.key) : super();
+
+  JsonIntMap.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonDoubleMap extends JsonPrimitiveMap<double> {
   JsonDoubleMap(super.key) : super();
+
+  JsonDoubleMap.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonBoolMap extends JsonPrimitiveMap<bool> {
   JsonBoolMap(super.key) : super();
+
+  JsonBoolMap.populated(super.key, super.val) : super.populated();
 }
 
 final class JsonObjectMap<T extends Json>
     extends JsonMap<T, Map<String, dynamic>> {
-  final T Function() parserConstructor;
+  late final T Function() parserConstructor;
 
   JsonObjectMap(super.key, this.parserConstructor) : super();
+
+  JsonObjectMap.populated(super.key, super.val) : super.populated() {
+    parserConstructor = () {
+      throw StateError('Cannot parse a populated object');
+    };
+  }
 
   @override
   T parseItem(Map<String, dynamic> item) => parserConstructor().._parse(item);
