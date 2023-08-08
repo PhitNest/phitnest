bbcdd8b232cea80b858ff23f058f0ef93fa2e8b4
John Siegel <jpsiegel8@gmail.com>
2023-08-07 22:47:15 -0400

    Perfected the JSON library

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -3,6 +3,6 @@ import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 import '../aws/aws.dart';
 import '../cache/cache.dart';
 import '../http/http.dart';
-import '../serializable.dart';
+import '../json.dart';
 
 part 'get_info.dart';
