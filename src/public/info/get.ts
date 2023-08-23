1fa69255f64020e4b44c0960eef493410812b75e
John Siegel <jpsiegel8@gmail.com>
2023-08-23 17:59:24 -0400

    Fixed api

--- src/public/info/get.ts
+++ src/public/info/get.ts
@@ -4,10 +4,14 @@ import { EnvironmentVars, handleRequest, Success } from "common/utils";
 export async function invoke(): Promise<APIGatewayProxyResult> {
   return await handleRequest(async () => {
     return new Success({
-      userPoolId: EnvironmentVars.userPoolId(),
-      userClientId: EnvironmentVars.userPoolClientId(),
-      adminPoolId: EnvironmentVars.adminPoolId(),
-      adminClientId: EnvironmentVars.adminPoolClientId(),
+      userPool: {
+        poolId: EnvironmentVars.userPoolId(),
+        clientId: EnvironmentVars.userPoolClientId(),
+      },
+      adminPool: {
+        poolId: EnvironmentVars.adminPoolId(),
+        clientId: EnvironmentVars.adminPoolClientId(),
+      },
       userIdentityPoolId: EnvironmentVars.userIdentityPoolId(),
       userBucketName: EnvironmentVars.userS3BucketName(),
     });
