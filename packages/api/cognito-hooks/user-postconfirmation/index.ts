88db18361311e0c44889a24b71d60d102499f193
John Siegel <jpsiegel8@gmail.com>
2023-09-04 12:57:08 -0400

    Debug

--- packages/api/cognito-hooks/user-postconfirmation/index.ts
+++ packages/api/cognito-hooks/user-postconfirmation/index.ts
@@ -1,14 +1,14 @@
 import { PostConfirmationTriggerEvent } from "aws-lambda";
 import {
-  EnvironmentVars,
+  // EnvironmentVars,
   ResourceNotFoundError,
   dynamo,
 } from "typescript-core/src/utils";
 import { getUserWithoutIdentity } from "typescript-core/src/repositories";
-import * as AWS from "aws-sdk";
+// import { CognitoIdentityServiceProvider } from "aws-sdk";
 
 export async function invoke(event: PostConfirmationTriggerEvent) {
-  AWS.config.update({ region: EnvironmentVars.region() });
+  // AWS.config.update({ region: EnvironmentVars.region() });
   // const cognitoIdentityProvider = new AWS.CognitoIdentityServiceProvider({
   //   apiVersion: "2016-04-18",
   // });
