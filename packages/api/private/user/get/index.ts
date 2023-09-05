b137caacd033163a05e50f2fb14943a9e8f2d1ac
John Siegel <jpsiegel8@gmail.com>
2023-09-05 16:55:11 -0400

    Fixed spelling error

--- packages/api/private/user/get/index.ts
+++ packages/api/private/user/get/index.ts
@@ -144,7 +144,7 @@ export async function invoke(
     return new Success({
       user: user,
       exploreUsers: exploreUsers,
-      friendshisps: friendships,
+      friendships: friendships,
       gym: gym,
     });
   });
