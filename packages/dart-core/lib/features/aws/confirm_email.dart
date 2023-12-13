c3c86b98f4aad5fb00221adc3edb42bf70be5867
John Siegel <jpsiegel8@gmail.com>
2023-12-13 10:01:52 -0500

    Fixed issue #88: readability of verification code error message

--- packages/dart-core/lib/features/aws/confirm_email.dart
+++ packages/dart-core/lib/features/aws/confirm_email.dart
@@ -2,6 +2,9 @@ import 'package:amazon_cognito_identity_dart_2/cognito.dart';
 
 import '../logger.dart';
 
+/// Verifies a users email address using a 6-digit pin code sent by AWS Cognito
+///
+/// Returns null if successful, otherwise an error message
 Future<String?> confirmEmail({
   required CognitoUser user,
   required String code,
@@ -12,9 +15,10 @@ Future<String?> confirmEmail({
     } else {
       return 'Failed to confirm email';
     }
-  } catch (e) {
-    error(e.toString());
-    return e.toString();
+  } on CognitoClientException catch (e) {
+    final errorMessage = e.message ?? e.toString();
+    error(errorMessage);
+    return errorMessage;
   }
 }
 
