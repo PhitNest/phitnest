35abb73bb0336c57563c3bb936b4045d4d6b5a95
John Siegel <jpsiegel8@gmail.com>
2023-12-23 15:44:48 -0500

    Removing aws code from git hist

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  singleQuote: false,
-  trailingComma: 'es5',
-  printWidth: 80,
-  endOfLine: 'auto',
-};
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { App } from "aws-cdk-lib";
-import { PhitnestStack } from "./phitnest-stack";
-
-const app = new App();
-new PhitnestStack(app);
