136fc3dfb87be34fb8e021278bbc4ddeee09798c
John Siegel <jpsiegel8@gmail.com>
2024-01-10 23:38:07 -0500

    Removed jest helpers

--- packages/jest-helpers/src/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-import { getTestOutputPath } from "./test-helpers";
-import * as fs from "fs";
-
-beforeEach(() => {
-  // Clear the test output directory before each test
-  fs.rmSync(getTestOutputPath(), { recursive: true, force: true });
-});
